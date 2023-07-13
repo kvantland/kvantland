9c3248b8b1b15fdeed4c5146a52631bf09def54f
numzero <numzer0@yandex.ru>
2023-07-13 19:25:48 +0300

    Запись схемы БД в SQL

---
+++ db/schema.sql
@@ -0,0 +1,65 @@
+drop schema if exists Квантландия cascade;
+create schema Квантландия;
+
+set search_path to Квантландия, public;
+
+create table Тип (
+	тип int primary key generated always as identity
+	, код text not null unique
+	, название text not null unique
+);
+
+create table Город (
+	город int primary key generated always as identity
+	, название text not null unique
+	, положение point
+);
+
+create table Группа (
+	группа int primary key generated always as identity
+	, город int not null references Город on delete cascade
+	, баллы int not null
+	, положение point
+	, unique (город, баллы)
+);
+
+create table Задача (
+	задача int primary key generated always as identity
+	, группа int not null references Группа on delete cascade
+	, тип int not null references Тип on delete cascade
+	, название text not null unique
+	, видимость bool not null default false
+);
+
+create table Вариант (
+	вариант int primary key generated always as identity
+	, задача int not null references Задача on delete cascade
+	, описание text not null
+	, содержание jsonb not null
+);
+
+create table Ученик (
+	ученик int primary key generated always as identity
+	, логин text not null unique
+	, пароль bytea
+	, имя text
+	, счёт int not null default 10
+);
+
+create table ТекущаяЗадача (
+	ученик int not null unique references Ученик on delete cascade,
+	вариант int not null references Вариант on delete cascade,
+	primary key (ученик, вариант)
+);
+
+create table ЗакрытиеЗадачи (
+	ученик int not null references Ученик on delete cascade,
+	задача int not null references Задача on delete cascade,
+	primary key (ученик, задача)
+);
+
+insert into Тип (код, название) values
+('radio', 'С выбором ответа'),
+('integer', 'С целочисленным ответом');
+
+-- kate: syntax SQL (PostgreSQL);
