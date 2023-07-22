a4fd1a6a51163489ab0c8a2479e3fd4c2412051e
numzero <numzer0@yandex.ru>
2023-07-22 21:26:59 +0300

    Карты городов

--- www/town/index.py
+++ www/town/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import random
 import json
 import os
 from sys import stdin
@@ -20,18 +21,31 @@ def show_town(cur):
 	print('<link rel="stylesheet" type="text/css" href="/static/master.css">')
 	print('<main>')
 	print(f'<h1>{название}</h1>')
+	print('<svg class="map" viewBox="0 0 100 100">')
+	print(f'<image href="/static/map/town-{город}.jpg" width="100" height="100" />')
 	cur.execute('select группа, положение, баллы from Группа where город = %s', (город,))
 	for группа, положение, баллы in cur.fetchall():
-		# TODO: положение
-		print(f'<a href="/problem/next.py?group={группа}">{баллы}</a>')
+		try:
+			x, y = положение
+		except TypeError:
+			μ, σ = 50.0, 15.0
+			x = random.normalvariate(μ, σ)
+			y = random.normalvariate(μ, σ)
+		print(f'<a class="level" transform="translate({x} {y})" href="/problem/next.py?group={группа}">')
+		print(f'<circle class="level-icon" r="0.65em" />')
+		print(f'<text class="level-value">{баллы}</text>')
+		print(f'</a>')
+	print('</svg>')
 	print('</main>')
 
 try:
 	import psycopg
+	from psycopg.types.composite import CompositeInfo, register_composite
 
 	город = int(query['id'])
 
 	with psycopg.connect('postgres://kvantland:quant@127.0.0.1') as con:
+		register_composite(CompositeInfo('point', 600, 1017, field_names=('x', 'y'), field_types=(701, 701)))
 		with con.cursor() as cur:
 			show_town(cur)
 except Exception as e:
