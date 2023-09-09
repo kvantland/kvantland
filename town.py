931f31bbafec3d4481753e44b8c2fca94f075aa3
numzero <numzer0@yandex.ru>
2023-09-10 00:01:39 +0300

    GH-45: mark solved/failed problems

--- town.py
+++ town.py
@@ -25,20 +25,24 @@ def show_town(db, town):
 	yield f'<image href="/static/map/town-{town}.jpg" width="1280" height="720" preserveAspectRatio="xMidYMid meet" />'
 
 	if user_id is not None:
-		db.execute('select вариант, положение, баллы, название, not ответ_дан открыта from ДоступнаяЗадача join Вариант using (вариант) join Задача using (задача) where город = %s and ученик = %s', (town, user_id))
+		db.execute('select вариант, положение, баллы, название, ответ_верен from ДоступнаяЗадача join Вариант using (вариант) join Задача using (задача) where город = %s and ученик = %s', (town, user_id))
 	else:
-		db.execute('select null, положение, баллы, название, true открыта from Задача where город = %s', (town,))
-	for вариант, положение, баллы, название, открыта in db.fetchall():
+		db.execute('select null, положение, баллы, название, null from Задача where город = %s', (town,))
+	for вариант, положение, баллы, название, ответ_верен in db.fetchall():
 		try:
 			x, y = положение
 		except TypeError:
 			μ, σ = 50.0, 15.0
 			x = random.normalvariate(μ, σ)
 			y = random.normalvariate(μ, σ)
-		tag = 'a' if открыта else 'g'
+		status = {
+			None: 'open',
+			True: 'solved',
+			False: 'failed',
+		}[ответ_верен]
 		link = f'/problem/{вариант}/' if user_id is not None else f'/login?path=/town/{town}/'
-		yield f'<{tag} href="{link}" class="level" transform="translate({x} {y})"><title>{название}</title>'
+		yield f'<a href="{link}" class="level level_{status}" transform="translate({x} {y})"><title>{название}</title>'
 		yield f'<circle class="level-icon" r="0.65em" />'
 		yield f'<text class="level-value">{баллы}</text>'
-		yield f'</{tag}>'
+		yield f'</a>'
 	yield '</svg>'
