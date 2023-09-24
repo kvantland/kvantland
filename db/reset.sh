ffbfa05a4c22fc59b22096aa32c2fa096cf2e648
numzero <numzer0@yandex.ru>
2023-09-24 14:09:36 +0300

    Read config.toml from reset.sh

--- db/reset.sh
+++ db/reset.sh
@@ -5,6 +5,11 @@ set -e
 dir="$(dirname "$(realpath "$0")")"
 postgres="$1"
 
+if [ -z "$postgres" ]; then
+	# read from config.toml
+	postgres="$(PYTHONPATH="$dir/.." python -c "from config import config; print(config['db']['url'])")"
+fi
+
 psql "$postgres" -1 -f "$dir/schema.sql"
 psql "$postgres" -1 -f "$dir/base-data.sql"
 psql "$postgres" -1 -f "$dir/users.sql"
