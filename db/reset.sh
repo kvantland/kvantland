1bbe7010b96224f80921f550298781165e791a3b
Vitaliy <numzer0@yandex.ru>
2023-09-26 20:30:15 +0300

    Use Python 3 explicitly

--- db/reset.sh
+++ db/reset.sh
@@ -7,7 +7,7 @@ postgres="$1"
 
 if [ -z "$postgres" ]; then
 	# read from config.toml
-	postgres="$(PYTHONPATH="$dir/.." python -c "from config import config; print(config['db']['url'])")"
+	postgres="$(PYTHONPATH="$dir/.." python3 -c "from config import config; print(config['db']['url'])")"
 fi
 
 psql "$postgres" -1 -f "$dir/schema.sql"
