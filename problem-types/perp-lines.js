705ece78264270a7a07340869d74a7770fc8e999
dicher74 <dicher74@gmail.com>
2023-09-27 14:54:17 +0300

    Perp-lines 2.0

--- problem-types/perp-lines.js
+++ problem-types/perp-lines.js
@@ -4,30 +4,16 @@ var curr_point = '';
 var curr_point_name = '';
 
 for (let i = 0; i < points.length; i++){
-	points[i].onmouseover = function(e){
-		if (points[i].style.fill != 'red'){
-			points[i].style.fill = 'rgba(1, 0, 0, 0.5)';
-		}
-	};
-
-	points[i].onmouseout = function(e){
-		if (points[i].style.fill != 'red'){
-			points[i].style.fill = 'rgba(0, 0, 0, 0)';
-		}
-	};
-
 	points[i].onclick = function(e){
 		if (curr_point){
-			curr_point.style.fill = 'rgba(0, 0, 0, 0)';
-			curr_point_name = document.getElementById("curr_point_name");
+			curr_point.classList.remove('base_point');
 			curr_point_name.remove();
 		}
-		points[i].style.fill = 'red';
+		points[i].classList.add('base_point');
 		curr_point = points[i];
 		curr_point_name = document.createElement('div');
 		curr_point_name.className = "point_name";
 		curr_point_name.innerHTML = 'D';
-		curr_point_name.id = "curr_point_name";
 		var x = parseInt(curr_point.getAttribute('cx'), 10);
 		var y = parseInt(curr_point.getAttribute('cy'), 10);
 		curr_point_name.style.top = String(y - 26) + 'px';
@@ -37,20 +23,14 @@ for (let i = 0; i < points.length; i++){
 };
 
 var curr_form = document.getElementById('problem_form');
-var sender = document.createElement('input');
-sender.name = 'answer';
-sender.type= 'hidden';
-curr_form.append(sender);
-var x = 0;
-var y = 0;
-
+var sender = document.getElementsByName('answer')[0];
 
 
 document.querySelector('button').onclick = function(e){
-	x = parseInt(curr_point.getAttribute('cx'), 10);
-	y = parseInt(curr_point.getAttribute('cy'), 10);
+	var x = parseInt(curr_point.getAttribute('cx'), 10);
+	var y = parseInt(curr_point.getAttribute('cy'), 10);
 	var i = Math.floor((x - 6) / 40);
-	var j = Math.floor((y - 6) / 40);
+	var j = Math.floor((y - 7) / 40);
 	var answer = [j, i];
 	sender.value = answer.join('');
 }
--- problem-types/perp-lines.py
+++ problem-types/perp-lines.py
@@ -1,26 +1,29 @@
 def entry_form(data, kwargs):
 	point_names = ['A', 'B', 'C']
 	points = data['points']
+	yield '<input name="answer" type="hidden" />'
 	yield '<div class="plot_area">'
-	yield '<div class="plot">'
+	yield '<table class="plot">'
+	yield '<tbody class="plot_body">'
 	for i in range(8):
-		yield '<div class="plot_row">'
+		yield '<tr class="plot_row">'
 		for j in range(8):
-			yield '<div class="square"></div>'
-		yield '</div>'
-	yield '</div>'
+			yield '<td class="square"></td>'
+		yield '</tr>'
+	yield '</tbody>'
+	yield '</table>'
 	yield'<svg class="full_window">'
 	for i in range(9):
 		for j in range(9):
 			if [j, i] in points:
-				yield f'<circle class="base_point" cx="{i *40.8 + 6}px" cy="{j * 40.8 + 6}px" />'
+				yield f'<circle class="base_point" cx="{i * 40 + 6}px" cy="{j * 40 + 7}px" />'
 			else:
-				yield f'<circle class="point" cx="{i *40.8 + 6}px" cy="{j * 40.8 + 6}px" />'
+				yield f'<circle class="point" cx="{i *40 + 6}px" cy="{j * 40 + 7}px" />'
 	yield'</svg>'
 	for i in range(9):
 		for j in range(9):
 			if [j, i] in points:
-				yield f'<div class="point_name" style="top: {j *40.8 - 20}px; left: {i * 40.8 - 20}px"> {point_names[points.index([j, i])]} </div>'
+				yield f'<div class="point_name" style="top: {j * 40 - 20}px; left: {i * 40 - 20}px"> {point_names[points.index([j, i])]} </div>'
 	yield '</div>'
 
 def validate(data, answer):
