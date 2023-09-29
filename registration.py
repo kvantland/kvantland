a8245e6db2cf415ab054513c94af7473f323e9d3
dicher74 <dicher74@gmail.com>
2023-09-29 23:24:01 +0300

    Проверка существования логина

--- registration.py
+++ registration.py
@@ -8,12 +8,20 @@ def reg_from():
     redirect('/')
   yield from display_registration_form()
 
-def display_registration_form():
+def display_registration_form(err=None):
   yield '<!DOCTYPE html>'
   yield '<title> Регистрация — Квантландия </title>'
   yield '<link rel="stylesheet" type="text/css" href="/static/master.css">'
-  yield from nav.display_breadcrumbs(('/', 'Квантландия'))
+  if err:
+    yield '<dialog open="open" class="reg_dialog">'
+    yield '<p>К сожаление, пользователь с таким логином уже существует,</p>'
+    yield '<p align="left">попробуйте другой логин. </p>'
+    yield '<form method="dialog">'
+    yield '<button type="submit" class="dialog_button">Закрыть</button>'
+    yield '</form>'
+    yield '</dialog>'
   yield '<main>'
+  yield from nav.display_breadcrumbs(('/', 'Квантландия'))
   yield '<div class="reg_form">'
   yield '<div class = "reg_form_header"> Регистрация </div>'
   yield '<form class="reg" method="post">'
@@ -28,6 +36,8 @@ def display_registration_form():
   yield '<a href="/login"> Уже зарегестрированы? </a>'
   yield '</div>'
   yield '</div>'
+  yield '</main>'
+  yield '<script type="text/javascript" src ="/static/registration.js"></script>'
 
 def add_user(db, логин, пароль, имя, школа, класс):
   db.execute("insert into Ученик (логин, пароль, имя, школа, класс) values (%s, %s, %s, %s, %s) returning ученик", (логин, пароль, имя, школа, класс))
@@ -35,9 +45,21 @@ def add_user(db, логин, пароль, имя, школа, класс):
   db.execute("insert into ДоступнаяЗадача (ученик, вариант) select distinct on (задача) %s, вариант from Вариант order by задача, random();", (user, ))
   return user
 
+def check_login(db, логин):
+  db.execute("select ученик from Ученик where логин = %s", (логин,))
+  try:
+    (user,) = db.fetchall()
+  except ValueError:
+    return None
+  return user
+
 @route('/reg', method='POST')
 def login_attempt(db):
-  user = add_user(db, request.forms.login, request.forms.password, request.forms.name, request.forms.school, request.forms.clas)
-  do_login(user)
-  redirect('/')
+  if check_login(db, request.forms.login):
+    yield from display_registration_form('reg_error')
+  else:
+    yield from display_registration_form()
+    user = add_user(db, request.forms.login, request.forms.password, request.forms.name, request.forms.school, request.forms.clas)
+    do_login(user)
+    redirect('/')
   
---
+++ static/registration.js
@@ -0,0 +1,16 @@
+var dialogs = document.getElementsByClassName('reg_dialog');
+var dialog = '';
+var k = 0;
+
+if (dialogs.length > 0){
+	var dialog = dialogs[0];
+};
+
+if (dialog){
+	var blur = document.getElementsByTagName('main')[0];
+	var button = document.getElementsByClassName('dialog_button')[0];
+	blur.classList.add('blur_zone'); 
+	button.onclick = function(e){
+		blur.classList.remove('blur_zone')
+	}
+}
