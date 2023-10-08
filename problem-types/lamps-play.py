8e868f736a457b736b18b7bc51765b77b1a98bb9
numzero <numzer0@yandex.ru>
2023-10-08 13:14:10 +0300

    Переход «игры с лампочками» на SVG

--- problem-types/lamps-play.py
+++ problem-types/lamps-play.py
@@ -1,28 +1,26 @@
 import json
 
+MARGIN = 0.2
+
+def render_lamp_array(w: int, h: int, extra_attrs):
+	off = MARGIN
+
+	yield f'<svg class="grid" width="{2 * off + w}em" height="{2 * off + h}em">'
+	for y in range(h):
+		for x in range(w):
+			yield f'<rect {extra_attrs(x, y)} x="{off + x}em" y="{off + y}em" width="1em" height="1em" />'
+	yield '</svg>'
+
 def entry_form(data, kwargs):
 	w, h = data['width'], data['height']
 	switches = data['switches']
-	yield '<table class="grid">'
-	for y in range(h):
-		yield '<tr>'
-		for x in range(w):
-			yield f'<td id="lamp_{x}_{y}" class="lamp lamp_on">'
-	yield '</table>'
+	yield '<div class="real_lamps">'
+	yield from render_lamp_array(w, h, lambda x, y: f'id="lamp_{x}_{y}" class="lamp lamp_on"')
+	yield '</div>'
 	yield '<div class="lamp_bar">'
 	for k, switch in enumerate(switches):
 		yield '<div>'
-		yield '<table class="grid">'
-		assert len(switch) == h
-		for y, row in enumerate(switch):
-			assert len(row) == w
-			yield '<tr>'
-			for x, val in enumerate(row):
-				classes = 'lamp'
-				if val:
-					classes += ' lamp_on'
-				yield f'<td id="lamp_{x}_{y}" class="{classes}">'
-		yield '</table>'
+		yield from render_lamp_array(w, h, lambda x, y: 'class="lamp lamp_on"' if switch[y][x] else 'class="lamp"')
 		yield f'<button type="button" class="lamp_switch" data-id="{k}">{k + 1}</button>'
 		yield '</div>'
 	yield '</div>'
