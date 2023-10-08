57756ddbfc9e26d068346439974083804b72a37f
numzero <numzer0@yandex.ru>
2023-10-08 13:18:16 +0300

    Приведение в порядок задания доп. атрибутов

--- problem-types/lamps-play.py
+++ problem-types/lamps-play.py
@@ -8,19 +8,20 @@ def render_lamp_array(w: int, h: int, extra_attrs):
 	yield f'<svg class="grid" width="{2 * off + w}em" height="{2 * off + h}em">'
 	for y in range(h):
 		for x in range(w):
-			yield f'<rect {extra_attrs(x, y)} x="{off + x}em" y="{off + y}em" width="1em" height="1em" />'
+			attrs = ' '.join(f'{key}="{value}"' for key, value in extra_attrs(x, y).items())
+			yield f'<rect {attrs} x="{off + x}em" y="{off + y}em" width="1em" height="1em" />'
 	yield '</svg>'
 
 def entry_form(data, kwargs):
 	w, h = data['width'], data['height']
 	switches = data['switches']
 	yield '<div class="real_lamps">'
-	yield from render_lamp_array(w, h, lambda x, y: f'id="lamp_{x}_{y}" class="lamp lamp_on"')
+	yield from render_lamp_array(w, h, lambda x, y: {'id': f"lamp_{x}_{y}", 'class': "lamp lamp_on"})
 	yield '</div>'
 	yield '<div class="lamp_bar">'
 	for k, switch in enumerate(switches):
 		yield '<div>'
-		yield from render_lamp_array(w, h, lambda x, y: 'class="lamp lamp_on"' if switch[y][x] else 'class="lamp"')
+		yield from render_lamp_array(w, h, lambda x, y: {'class': "lamp lamp_on" if switch[y][x] else "lamp"})
 		yield f'<button type="button" class="lamp_switch" data-id="{k}">{k + 1}</button>'
 		yield '</div>'
 	yield '</div>'
