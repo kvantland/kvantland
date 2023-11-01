e33ec5d9638f6a0d6353637794b86219c69e4676
dicher74 <dicher74@gmail.com>
2023-11-01 19:21:20 +0300

    Личные кабинеты

--- user.py
+++ user.py
@@ -9,7 +9,7 @@ def display_banner(db):
 	path_arg = escape(quote('?'.join(request.urlparts[2:4]), safe=''))
 	user = current_user()
 	yield '<nav class="user_nav">'
-	yield '<ul>'
+	yield '<ul class="flex_ul">'
 	if user != None:
 		db.execute('select coalesce(имя, логин), счёт from Ученик where ученик = %s', (user,))
 		(login, money), = db.fetchall()
@@ -18,6 +18,7 @@ def display_banner(db):
 	yield f'<li><a href="/rules">Правила</a>'
 	if user != None:
 		yield f'<li><a class="login" href="/logout?path={path_arg}">Выйти</a>'
+		yield f'<li><div class="acc_icon"> <a class="acc_href" href="/acc"></a> </div>'
 	else:
 		yield f'<li><a class="login" href="/login?path={path_arg}">Войти</a>'
 	yield '</ul>'
