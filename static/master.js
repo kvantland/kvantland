0eb2c3f13e39b0ca7646a2beb4cd4237940724c4
dicher74 <dicher74@gmail.com>
2023-12-06 04:05:49 +0300

    Разрешение проблем с совместимостью в задаче "Перпендикулярные прямые"

--- static/master.js
+++ static/master.js
@@ -3,12 +3,15 @@ window.confirm_answer = confirm_answer // HACK make it available to non-modules,
 function confirm_answer(ev) {
 	if (!confirm('Готовы отправить ответ? Передумать будет нельзя!'))
 		ev.preventDefault()
+	else
+		save_progress()
 }
 
 function confirm_hint(ev) {
 	if (!confirm('Вы точно хотите получить подсказку? Её стоимость 1 квантик.'))
 		ev.preventDefault()
-	hint_save()
+	else
+		hint_save()
 }
 
 for (let form of document.querySelectorAll("form.problem")) {
@@ -34,16 +37,17 @@ function hint_save(){
 	if (interactive)
 		localStorage.setItem("solution", interactive.outerHTML);
 	else
-		localStorage.setItem("solution", form.outerHTML)
+		localStorage.setItem("solution", form.outerHTML);
 	localStorage.setItem("answer", document.querySelector("input[name='answer']").value);
 }
 
 function save_progress(){
+	var progress = document.querySelector("input[name='progress']");
 	var interactive = document.querySelector('#interactive_problem_form');
 	if (interactive)
 		progress.value = document.querySelector('#interactive_problem_form').outerHTML;
 	else
-		progress.value = querySelector('#problem_form').outerHTML;
+		progress.value = document.querySelector('#problem_form').outerHTML;
 }
 
 function update_load(){
@@ -69,6 +73,6 @@ function update_load(){
 	}
 }
 
-send_button.addEventListener('click', save_progress);
+send_button.addEventListener('click', confirm_answer);
 
 document.addEventListener("DOMContentLoaded", update_load())
