bbbfe3d6e852dd23926bb206fda9b694c59066b3
dicher74 <dicher74@gmail.com>
2023-12-17 18:47:24 +0300

    Доработка внешнего вида задачи

--- problem-types/five_coins.py
+++ problem-types/five_coins.py
@@ -2,6 +2,7 @@ def entry_form(data, kwargs):
 	scale_1 = 0.7
 	cup_yr = 100 * 0.4
 	cup_xr = 300 * 0.4
+	ellipse_a = cup_yr / cup_xr
 	cup_under_xr = cup_xr
 	cup_under_yr = cup_yr * 1.5
 	rope_length = 400 * 0.4
@@ -13,21 +14,28 @@ def entry_form(data, kwargs):
 	base_plank_up = 30
 	basement_width = 250
 	basement_hight = 80
-	base_inside_yr = 100 * 0.4
-	base_inside_xr = 200 * 0.4
-	coin_size = 80
-	coin_pad = 10
+	base_inside_yr = cup_yr * 0.7
+	base_inside_xr = cup_xr * 0.7
+	coin_size = 70
 	drag_pad = 30
+	weight = [10, 20, 20, 30, 60]
+	answer_zone_height = 120
+	container_height = answer_zone_height
+	container_width = 100
+	container_pad = 20
+	coin_pad = container_width - coin_size + container_pad
+	cont_a = 0.25
+	answer_zone_pad = 20
 	svg_width = (plank_length + cup_xr * 2) * scale_1
-	svg_height = (base_plank_width / 4 + base_plank_height + basement_hight + basement_width / 4) * scale_1 + drag_pad + coin_size
+	svg_height = (base_plank_width / 2 * ellipse_a + base_plank_height + basement_hight + basement_width / 2 * ellipse_a) * scale_1 + drag_pad + coin_size + answer_zone_height + answer_zone_pad
 	yield '<input name="answer" type="hidden" />'
 	yield '<div class="plot_area">'
 	yield f'<svg version="1.1" width="{svg_width}" height="{svg_height}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
 	yield '<defs>'
 
 	yield '<linearGradient id="cupShadowInside">'
-	yield '<stop offset="0%" stop-color="#7B6F01" />'
-	yield '<stop offset="100%" stop-color="#373200" />'
+	yield '<stop offset="0%" stop-color="#373200" />'
+	yield '<stop offset="100%" stop-color="#7B6F01" />'
 	yield '</linearGradient>'
 
 	yield '<linearGradient id="cupShadowOutside">'
@@ -53,7 +61,7 @@ def entry_form(data, kwargs):
 	yield '<stop offset="50%" stop-color="#fff2af" />'
 	yield '<stop offset="100%" stop-color="gold" />'
 	yield '</linearGradient>'
-	#fff2af
+
 	yield '</defs>'
 	yield f'<g class="scales" transform="scale({scale_1}) translate({cup_xr} {base_plank_up + base_plank_width / 4})">'
 	yield f"""<rect class="plank" 
@@ -65,7 +73,7 @@ def entry_form(data, kwargs):
 	yield f"""<path class="base_outside" transform="translate({-basement_width / 2} {base_plank_height - base_plank_up})"
 				d="M 0 0 
 				V {basement_hight}
-				A {basement_width / 2} {basement_width / 4} 0 0 0 {basement_width} {basement_hight}
+				A {basement_width / 2} {basement_width / 2 * ellipse_a} 0 0 0 {basement_width} {basement_hight}
 				V 0
 				H 0
 				Z" />"""
@@ -73,7 +81,7 @@ def entry_form(data, kwargs):
 	 			cx="0"
 	 			cy="{base_plank_height - base_plank_up}"
 	 			rx="{basement_width / 2}"
-	 			ry="{basement_width / 4}" />"""
+	 			ry="{basement_width / 2 * ellipse_a}" />"""
 	yield f"""<ellipse class="base_inside"
 				cx="0"
 				cy="{base_plank_height - base_plank_up}"
@@ -83,9 +91,9 @@ def entry_form(data, kwargs):
 				transform="translate({-base_plank_width / 2} {-base_plank_up})"
 				d="M 0 0
 	 			V {base_plank_height}
-	 			A {base_plank_width / 2} {base_plank_width / 4} 0 0 0 {base_plank_width} {base_plank_height}
+	 			A {base_plank_width / 2} {base_plank_width / 2 * ellipse_a} 0 0 0 {base_plank_width} {base_plank_height}
 	 			V 0
-	 			A {base_plank_width / 2} {base_plank_width / 4} 0 0 0 0 0"
+	 			A {base_plank_width / 2} {base_plank_width / 2 * ellipse_a} 0 0 0 0 0"
 	 			Z" />"""
 	yield f"""<circle class="hinge center" transform="translate(0 {plank_width / 2})"
 			cx="0"
@@ -109,13 +117,13 @@ def entry_form(data, kwargs):
 	yield f"""<line 
 				x1="{cup_xr}"
 				y1="0"
-				x2="0"
-				y2="{rope_length + cup_yr}" />"""
+				x2="{cup_xr - 1.5 * cup_xr / (3 ** 0.5)}"
+				y2="{rope_length + cup_yr * 1.5}" />"""
 	yield f"""<line 
 				x1="{cup_xr}"
 				y1="0"
-				x2="{cup_xr * 2}"
-				y2="{rope_length + cup_yr}" />"""
+				x2="{cup_xr + 1.5 * cup_xr / (3 ** 0.5)}"
+				y2="{rope_length + cup_yr * 1.5}" />"""
 	yield f"""<circle class="hinge left"
 				cx="{cup_xr}"
 				cy="0"
@@ -138,24 +146,33 @@ def entry_form(data, kwargs):
 	yield f"""<line 
 				x1="{cup_xr}"
 				y1="0"
-				x2="0"
-				y2="{rope_length + cup_yr}" />"""
+				x2="{cup_xr - 1.5 * cup_xr / (3 ** 0.5)}"
+				y2="{rope_length + cup_yr * 1.5}" />"""
 	yield f"""<line 
 				x1="{cup_xr}"
 				y1="0"
-				x2="{cup_xr * 2}"
-				y2="{rope_length + cup_yr}" />"""
+				x2="{cup_xr + 1.5 * cup_xr / (3 ** 0.5)}"
+				y2="{rope_length + cup_yr * 1.5}" />"""
 	yield f"""<circle class="hinge right"
 				cx="{cup_xr}"
 				cy="0"
 				r="{hinge_r}"/>"""
 	yield '</g>'
 	yield '</g>'
-	yield f'<g class="drag_zone" transform="translate(0 {svg_height - coin_size})">'
+	yield f'<g class="drag_zone" transform="translate({(container_width - coin_size) / 2} {svg_height - coin_size - answer_zone_height - answer_zone_pad})">'
 	for i in range(1, 6):
-		yield f'<g class="coin num_{i}" title="{i}" transform="translate({(coin_size + coin_pad) * (i - 1)} 0)">'
+		yield f'<g class="coin num_{i}" transform="translate({(coin_size + coin_pad) * (i - 1)} 0)">'
+		yield f'<title> {i} </title>'
 		yield f'<circle cx="{coin_size / 2}" cy="{coin_size / 2}" r="{coin_size / 2}" />'
 		yield f'<text class="coin_text" x="{coin_size / 2}" y="{coin_size / 2}"> {i} </text>'
+		yield '</g>'
+	yield '</g>'
+	yield f'<g class="answer_zone" transform="translate(0 {svg_height - answer_zone_height})">'
+	for i in range(1, 6):
+		yield f'<g class="coin_container" transform="translate({(container_width + container_pad) * (i - 1)} 0)">'
+		yield f'<rect class="coin_container_name" x="0" y="0" width="{container_width}" height="{container_height * cont_a}"/>'
+		yield f'<text class="coin_container_name" x="{container_width / 2}" y="{container_height * cont_a / 2}"> {weight[i - 1]} граммов </text>'
+		yield f'<rect class="coin_container_drag" x=0 y="{container_height * cont_a}" width="{container_width}" height="{container_height * (1 - cont_a)}">'
 		yield '</g>'
 	yield '</g>'
 	yield '</svg>'
