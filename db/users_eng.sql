5d99ef25012f498c4dbda7220249836bfb5bd628
Vladislav Aksenov <vlador3et@gmail.com>
2023-12-19 12:30:31 -0500

    Корректировка user_eng

--- db/users_eng.sql
+++ db/users_eng.sql
@@ -9,10 +9,10 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-COPY "Kvantland"."User" ("user", "login", "password", "name", "score") FROM stdin;
+COPY "Kvantland"."Student" ("student", "login", "password", "name", "score") FROM stdin;
 1	zumen	$pbkdf2-sha256$29000$trZWqjXmXKtVSqn13puTkg$KS32OoYadv3oJ/Pg6u1/9MASbb/x5PamIRaAOie0jOw	\N	10
 2	testovich	$pbkdf2-sha256$29000$FIJQai0F4Dwn5Px/b83Zmw$yIpK7wY89GTR7tzUD4aDV5NyOOovPsQHxiAdc0Z0UGM	\N	10
 3	testerskiy	$pbkdf2-sha256$29000$b43xvtf6/5.zVkopxdh7Lw$KJPMq/0glM8ZbL8HwzaUJHU0xV6rJRHoaUDfBwFoS5E	\N	10
 \.
 
-SELECT pg_catalog.setval('"Kvantland"."User_user_seq"', 3, true);
+SELECT pg_catalog.setval('"Kvantland"."Student_student_seq"', 3, true);
