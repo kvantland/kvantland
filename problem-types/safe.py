9634730e91b88289a8cb1ee0b33f308fbf61e5e0
Vladislav Aksenov <vlador3et@gmail.com>
2024-01-05 18:15:13 -0500

    Добавление вариаций для задачи Как открыть сейф. Изменение стиля и добавление пошагового интерфейса

--- problem-types/safe.py
+++ problem-types/safe.py
@@ -8,10 +8,11 @@ def entry_form(data, kwargs):
     inner_side = side - line_width # длина стороны квадрата без границ 
     board_side = line_width + size * side # длина стороны доски
     pad = 6 # расстояние между доской и зоной перетаскивания
-    plot_width = pad + board_side + 2 * inner_side 
+    plot_width = board_side
     plot_height = line_width + 3 * side
     yield '<input name="answer" type="hidden" />'
-    yield f'<svg version="1.1" class="plot_area" width="{plot_width}" height="{plot_height}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+    yield '<div class="plot_area">'
+    yield f'<svg version="1.1" width="{plot_width}" height="{plot_height}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
     for card in range(len(data["start"])):
         if data["start"][card] == '*':
             yield f'<rect class="colored top" x="{card * side + line_width}" y="{line_width}" width="{inner_side}" height="{inner_side}" column="{card}"/>'
@@ -27,8 +28,21 @@ def entry_form(data, kwargs):
             yield f'<rect class="passive gray" x="{card * side + line_width}" y="{1 * side + line_width}" width="{inner_side}" height="{inner_side}" column="{card}"/>'
             yield f'<rect class="passive bottom" x="{card * side + line_width}" y="{2 * side + line_width}" width="{inner_side}" height="{inner_side}" column="{card}"/>'
             yield f'<text class="number_value known" x="{card * side + ind + side / 2}" y="{1 * side + ind + side / 2}" column="{card}">{data["start"][card]}</text>'
-    yield f'<image class="reload" x = "{pad + board_side}" y="{line_width}" height="{inner_side}" width="{inner_side}" href="/static/reload.png" />'
+    #yield f'<text style="font-size: {inner_side * 0.4}px" x="{ + board_side + 2 * inner_side}" y="{inner_side * 1.2}">Попыток осталось: {data["left"]}</text>'
+    #yield f'<image class="reload" x = "{pad + board_side}" y="{line_width}" height="{inner_side}" width="{inner_side}" href="/static/reload.png" />'
     yield '</svg>'
+    yield '<div class="interface_zone">'
+    yield '<button type="button" class="check"> Проверить </button>'
+    yield '<button type="button" class="reload"> Сбросить </button>'
+    yield '<div class="remaining_checks">'
+    yield f"<p> Осталось проверок: {max(0, data['left'] - 0)}<p>"
+    yield '</div>'
+    yield '</div>'
+    yield '</div>'
+
+
 
 def validate(data, answer):
     return answer in data['correct']
+
+WITHOUT_BUTTONS = True
