99969cb818c62e261f031a27c0c493f150ce00f8
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-01-11 00:17:10 -0500

    Изменение монет и добавление галочек для задачи лабиринт

--- problem-types/labyrinth.py
+++ problem-types/labyrinth.py
@@ -7,9 +7,15 @@ def entry_form(data, kwargs):
     inner_side = side - line_width # длина стороны квадрата без границ 
     board_width = line_width + width * side + ind # длина стороны доски
     board_height = line_width + height * side + ind
-
     yield '<input name="answer" type="hidden" />'
     yield f'<svg version="1.1" class="plot_area" width="{board_height}" height="{board_width}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+    yield '<defs>'
+    yield '<linearGradient id="coinShadow" x1="0" x2="1" y1="0" y2="1">'
+    yield '<stop offset="0%" stop-color="gold" />'
+    yield '<stop offset="50%" stop-color="#fff2af" />'
+    yield '<stop offset="100%" stop-color="gold" />'
+    yield '</linearGradient>'
+    yield '</defs>'
     yield f'<g transform="translate({board_width + side} 0 )rotate(90)">'
     for y in range(0, height + 1):
         yield f'<line class="grid_line" x1="{ind}" y1 = "{ind + y * side}" x2="{ind + width * side}" y2="{ind + y * side}" stroke-width="{line_width}"/>'
@@ -27,8 +33,8 @@ def entry_form(data, kwargs):
     for x in range(0, width):
         for y in range(0, height):
             if [x, y] in data['cur']:
-                yield f'<g class="star" transform="translate({x * side + ind} {y * side + ind})">'
-                yield f'<circle cx="{side / 2}" cy="{side / 2}" r="{15}" fill="yellow" stroke="orange" stroke-width="{5}"/>'
+                yield f'<g class="coin" transform="translate({x * side + ind} {y * side + ind})">'
+                yield f'<circle cx="{side / 2}" cy="{side / 2}" r="{15}"/>'
                 yield '</g>'
             yield f'<rect class="free square" row="{y}" column="{x}" x="{x * side + line_width}" y="{y * side + line_width}" width="{inner_side}" height="{inner_side}" />'
     yield f'</g>'
