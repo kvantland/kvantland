ac72cf0e703487d3f4f08bf5f94acbfbc8e3bcb2
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-01-16 01:18:12 -0500

    Стилистические правки в задаче Как открыть сейф

--- problem-types/safe.py
+++ problem-types/safe.py
@@ -4,7 +4,9 @@ def entry_form(data, kwargs):
     size = 10 # число клеточек в строке
     line_width = 2 # ширина линий 
     ind = line_width / 2 # отступ
-    side = 60 # длина стороны квадрата с границами
+    side = 50 # длина стороны квадрата с границами
+    pad = 50
+    damp = pad / 2
     inner_side = side - line_width # длина стороны квадрата без границ 
     board_side = line_width + size * side + line_width # длина стороны доски
     plot_height = line_width + 4 * side
@@ -18,25 +20,26 @@ def entry_form(data, kwargs):
     yield '<stop offset="100%" stop-color="gray" />'
     yield '</linearGradient>'
     yield '</defs>'
-    yield f'<line class="grid_line" x1="{ind}" y1 = "{ind + 0 * side + side}" x2="{ind + size * side}" y2="{ind + 0 * side + side}" stroke-width="{line_width}"/>'
-    yield f'<line class="grid_line" x1="{ind}" y1 = "{2 * side + side - ind}" x2="{ind + size * side}" y2="{ 2 * side + side - ind}" stroke-width="{line_width}"/>'
+    yield f'<rect class="border" x="{damp / 2}" y="{side / 2}" width="{damp + line_width + size * side}" height="{line_width + 3 * side}"/>'
+    yield f'<line class="grid_line" x1="{ind + damp}" y1 = "{ind + 0 * side + side}" x2="{ind + size * side + damp}" y2="{ind + 0 * side + side}" stroke-width="{line_width}"/>'
+    yield f'<line class="grid_line" x1="{ind + damp}" y1 = "{2 * side + side - ind}" x2="{ind + size * side + damp}" y2="{ 2 * side + side - ind}" stroke-width="{line_width}"/>'
     for x in range(0, size + 1):
-        yield f'<line class="grid_line" x1="{ind + x * side}" y1 = "{ind + side}" x2="{ind + x * side}" y2="{side * 3 - ind}" stroke-width="{line_width}"/>'
+        yield f'<line class="grid_line" x1="{ind + x * side + damp}" y1 = "{ind + side}" x2="{ind + x * side + damp}" y2="{side * 3 - ind}" stroke-width="{line_width}"/>'
     for card in range(len(data["start"])):
         if data["start"][card] == '*':
-            yield f"""<path class="active top" column="{card}" d=" M {card * side + line_width},{side - ind}
+            yield f"""<path class="active top" column="{card}" d=" M {card * side + line_width + damp},{side / 2 - side / 6}
                         l {side / 2},{-side / 3}
                         l {side / 2},{side / 3}
                         z"/>"""
-            yield f'<rect class="passive gray" x="{card * side + line_width}" y="{1 * side + line_width}" width="{inner_side}" height="{inner_side * 2}" column="{card}"/>'
-            yield f"""<path class="active bottom" column="{card}" d=" M {card * side + line_width},{3 * side + ind}
+            yield f'<rect class="passive slot" x="{card * side + line_width + damp}" y="{1 * side + line_width}" width="{inner_side}" height="{inner_side * 2}" column="{card}"/>'
+            yield f"""<path class="active bottom" column="{card}" d=" M {card * side + line_width + damp},{3 * side + side / 2 + side / 6}
                         l {side / 2},{side / 3}
                         l {side / 2},{-side / 3}
                         z"/>"""
-            yield f'<text class="number_value unknown" x="{card * side + ind + side / 2}" y="{2 * side + ind}" column="{card}">{data["start"][card]}</text>'
+            yield f'<text class="number_value unknown" x="{card * side + ind + side / 2 + damp}" y="{2 * side + ind}" column="{card}">{data["start"][card]}</text>'
         else:
-            yield f'<rect class="passive gray" x="{card * side + line_width}" y="{1 * side + line_width}" width="{inner_side}" height="{inner_side * 2}" column="{card}"/>'
-            yield f'<text class="number_value known" x="{card * side + ind + side / 2}" y="{2 * side + ind}" column="{card}">{data["start"][card]}</text>'
+            yield f'<rect class="passive slot" x="{card * side + line_width + damp}" y="{1 * side + line_width}" width="{inner_side}" height="{inner_side * 2}" column="{card}"/>'
+            yield f'<text class="number_value known" x="{card * side + ind + side / 2 + damp}" y="{2 * side + ind}" column="{card}">{data["start"][card]}</text>'
     yield '</svg>'
     yield '<div class="interface_zone">'
     yield '<button type="button" class="check"> Проверить </button>'
