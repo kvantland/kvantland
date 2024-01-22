041f149ecc6160379cf7effafecba9361d601702
dicher74 <dicher74@gmail.com>
2024-01-22 18:58:13 +0300

    Задачи не подсвечиваются

--- town.py
+++ town.py
@@ -24,10 +24,10 @@ def show_town(db, town):
 	yield '<svg version="1.1" class="map" viewBox="0 0 1280 720" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
 	yield f'<image href="/static/map/town-{town}.jpg" width="1280" height="720" preserveAspectRatio="xMidYMid meet" />'
 
-	db.execute('select вариант, положение, баллы, название, ответ_верен from ДоступнаяЗадача join Вариант using (вариант) join Задача using (задача) where город = %s', (town, ))
+	db.execute('select вариант, положение, баллы, название from ДоступнаяЗадача join Вариант using (вариант) join Задача using (задача) where город = %s', (town, ))
 	
 	cur_position = []
-	for variant, position, points, name, ans_true in db.fetchall():
+	for variant, position, points, name in db.fetchall():
 		try:
 			x, y = position
 			if cur_position == position:
@@ -37,11 +37,7 @@ def show_town(db, town):
 			μ, σ = 50.0, 15.0
 			x = random.normalvariate(μ, σ)
 			y = random.normalvariate(μ, σ)
-		status = {
-			None: 'open',
-			True: 'solved',
-			False: 'failed',
-		}[ans_true]
+		status = 'open'
 		link = f'/problem/{variant}/0/' 
 		yield f'<a xlink:href="{link}" class="level level_{status}" transform="translate({x} {y})"><title>{name}</title>'
 		yield f'<circle class="level-icon" r="0.65em" />'
