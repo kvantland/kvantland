6c53ece817eb3709829c4d4b860e83307ec47f57
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-01-29 12:11:47 -0500

    Добавление работающей кнопки техподдержки

--- footer.py
+++ footer.py
@@ -1,9 +1,11 @@
+from config import config
+
 def display_problem():
 	yield f'<div class="problem_footer_border">'
 	yield f'<div class="problem_footer">'
 	yield f'<div class="problem_footer_text">Если возникли технические проблемы, напишите в нашу службу поддержки</div>'
-	yield f'<div class="support_button_border">'
-	yield f'<div class="support_button">support@kvantland.com</div>'
-	yield f'</div>'
+	yield f'<a class="support_button_border" href="mailto:{config["contacts"]["support_email"]}">'
+	yield f'<div class="support_button">{config["contacts"]["support_email"]}</div>'
+	yield f'</a>'
 	yield f'</div>'
 	yield f'</div>'
