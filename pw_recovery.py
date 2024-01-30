f932cd08c97a614d9f048cb01ac426ef9d1e3945
dicher74 <dicher74@gmail.com>
2024-01-30 22:42:26 +0300

    Доработка заготовки

--- pw_recovery.py
+++ pw_recovery.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 from bottle import route, redirect, request, response
 from passlib.hash import pbkdf2_sha256 as pwhash
@@ -6,6 +7,11 @@ import json
 import hmac
 import urllib.parse
 import sys
+import email.message
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from html import escape
 
 from login import do_login, current_user
 from config import config
@@ -52,52 +58,69 @@ def show_send_message():
 	yield '<title>Восстановление пароля</title>'
 	yield '<link rel="stylesheet" type="text/css" href="/static/master.css">'
 	yield from nav.display_breadcrumbs(('/', 'Квантландия'), ('/login', 'Войти'))
-	yield '<main>'
-	yield '<div style="margin: 100px 50px 0 50px">'
-	yield '<p> Здравствуйте, Чертоляс Дмитрий Андреевич! </p>'
-	yield '<p> Недавно был получен запрос на изменение пароля вашей учетной записи. Если вы запрашивали это изменение пароля, нажмите на ссылку ниже для установки нового пароля: </p>'
-	yield '<div style="text-align:center; color:white; background-color:blue; display:block; padding:3px 10px 3px 10px; width:500px; margin: 50px auto 60px auto"> <p> Нажмите здесь, чтобы изменить пароль </p> </div>'
-	yield '<p> Если вам не нужно менять пароль, просто проигнорируйте данное сообщение. </p>'
-	yield '<p> С уважением, команда Kvantland </p>'
-	yield '</div>'
-	yield '</main>'
+	yield '<div> Письмо с восстановлением пароля отправлено на ваш адрес! </div>'
 
 @route('/pw_recovery', method="POST")
 def recovery_attempt(db):
-	email = request.forms.email
-	if not email:
+	_email = request.forms.email
+	if not _email:
 		return display_recovery_form(err="Не указан адрес электронной почты")
 	try:
-		db.execute('select student from Kvantland.Student where email=%s', (email, ))
-		(user, ), = db.fetchall()
+		yield from show_send_message()
+		db.execute('select student, name from Kvantland.Student where email=%s', (_email, ))
+		(user, name, ) = db.fetchall()[0]
 
 		check_user = current_user(db)
 		if check_user:
 			redirect('/')
 		else:
-			params = {
-			'email': email,
-			'redirect_uri': config['recovery']['redirect_uri'],
-			'token': hmac.new(_key.encode('utf-8'), email.encode('utf-8'), 'sha256').hexdigest()
-			}
-			send_recovery_request(params)
-			return show_send_message()
+			token = hmac.new(_key.encode('utf-8'), _email.encode('utf-8'), 'sha256').hexdigest()
+			link = f'''
+			http://{config['server']['host']}:{str(config['server']['port'])}/pw_recovery/new_password?token={token}&mail={_email} 
+			'''
+			host = config['recovery']['host']
+			port = config['recovery']['port']
+			login = config['recovery']['login']
+			password = config['recovery']['password']
+			sender = config['recovery']['sender']
+
+			server = smtplib.SMTP(f'{host}')
+			email_content =  f'''
+				<!DOCTYPE html>
+				<title>Восстановление пароля</title>
+				<div style="margin: 100px 50px 0 50px">
+				<p> Здравствуйте, {name}! </p>
+				<p> Недавно был получен запрос на изменение пароля вашей учетной записи. 
+				Если вы запрашивали это изменение пароля, нажмите на ссылку ниже для установки нового пароля: </p>
+				<a href="{link}"> 
+				<div style="text-align:center; color:white; background-color:blue; display:block; padding:3px 10px 3px 10px;
+				width:500px; margin: 50px auto 60px auto"> <p> Нажмите здесь, чтобы изменить пароль </p> </div>
+				</a>
+				<p> Если вам не нужно менять пароль, просто проигнорируйте данное сообщение. </p>
+				<p> С уважением, команда Kvantland </p>
+				</div>
+				</html>'''
+
+			msg = MIMEMultipart()
+			msg['Subject'] = 'Password recovery'
+			msg['From'] = sender
+			msg['To'] = _email
+			msg.attach(MIMEText(email_content, 'html'))
+
+			server.starttls()
+			server.login(str(login), str(password))
+			try:
+				server.sendmail(sender, [_email], msg.as_string())
+			except:
+				redirect('/')
+			finally:
+				server.quit()	
 	except ValueError:
 		return display_recovery_form(err="Неверный адрес электронной почты")
 
-
-def send_recovery_request(params):
-	pass
-
-@route('/pw_recovery/response', method="POST")
-def get_response():
-	params = request.query
-	redirect(f'/pw_recovery/new_password?{escape(urllib.parse.urlencode(params))}')
-
-
 @route('/pw_recovery/new_password')
 def display_new_password_form(err=None):
-	email = request.query['email']
+	email = request.query['mail']
 	token = request.query['token']
 	if not email or not token:
 		redirect('/')
@@ -145,7 +168,7 @@ def new_password_attempt(db):
 	min_size = config['reg']['min_password_size']
 	max_size = config['reg']['max_password_size']
 
-	mp_upper, tmp_lower, tmp_number = (0, 0, 0)
+	tmp_upper, tmp_lower, tmp_number = (0, 0, 0)
 	for s in password:
 		if s in alph_upper:
 			tmp_upper = 1
@@ -167,5 +190,5 @@ def new_password_attempt(db):
 
 def update_user(db, email, password):
 	db.execute('update Kvantland.Student set password=%s where email=%s returning student, login', (pwhash.hash(password), email, ))
-	(user, login), = db.fetchall()
+	(user, login, ) = db.fetchall()[0]
 	do_login(user, login)
