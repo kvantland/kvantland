50dc06cb99654e6b08eff8ed576c2dd30f17751a
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-01-30 19:52:03 -0500

    Добавление ссылок в футерах

--- footer.py
+++ footer.py
@@ -13,9 +13,9 @@ def display_starter():
 	yield '<div class="page_header">Будем на связи</div>'
 	yield '<div class="text">В наших социальных сетях мы регулярно публикуем интересные новости о проекте, анонсы ближайших событий и нестандартные задачки для всех!    </br></br>  Присоединяйтесь и будете всегда в курсе событий!</div>'
 	yield '<div class="button_area">'
-	yield '<div class="contact_button"> <img class="contact_icon" src="/static/design/icons/vk.svg" /> </div>'
-	yield '<div class="contact_button"> <img class="contact_icon" src="/static/design/icons/tg.svg" /> </div>'
-	yield '<div class="contact_button"> <img class="contact_icon" src="/static/design/icons/play.svg" /> </div>'
-	yield '<div class="contact_button"> <img class="contact_icon" src="/static/design/icons/email.svg" /> </div>'
+	yield f'<a class="contact_button" href="{config["contacts"]["vk_link"]}"> <img class="contact_icon" src="/static/design/icons/vk.svg" /> </a>'
+	yield f'<a class="contact_button" href="{config["contacts"]["tg_link"]}"> <img class="contact_icon" src="/static/design/icons/tg.svg" /> </a>'
+	yield f'<a class="contact_button" href="{config["contacts"]["youtube_link"]}"> <img class="contact_icon" src="/static/design/icons/play.svg" /> </a>'
+	yield f'<a class="contact_button" href="mailto:{config["contacts"]["support_email"]}"> <img class="contact_icon" src="/static/design/icons/email.svg" /> </a>'
 	yield '</div>'
 	yield '</div>'
