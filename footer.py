572d35ebf1f1c02856d991f11da3c8878fbc6e1a
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-01-30 19:37:28 -0500

    Корректировка css для футера

--- footer.py
+++ footer.py
@@ -1,10 +1,10 @@
 from config import config
 
 def display_problem():
-	yield f'<div class="problem_footer">'
-	yield f'<div class="problem_footer_text">Если возникли технические проблемы, напишите в нашу службу поддержки</div>'
-	yield f'<a class="support_button_border" href="mailto:{config["contacts"]["support_email"]}">'
-	yield f'<div class="support_button">{config["contacts"]["support_email"]}</div>'
+	yield f'<div class="support_area">'
+	yield f'<div class="text">Если возникли технические проблемы, напишите в нашу службу поддержки</div>'
+	yield f'<a class="border" href="mailto:{config["contacts"]["support_email"]}">'
+	yield f'<div class="button">{config["contacts"]["support_email"]}</div>'
 	yield f'</a>'
 	yield f'</div>'
 
