7869c0ef2abb0dd3af9e9b77b787037f1925ff97
dicher74 <dicher74@gmail.com>
2024-02-04 00:03:04 +0300

    Добавление дизайна для политики конфиденциальности

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from bottle import route, run, static_file, install
 
 import db
 import nav
-import land, town, problem, login, loginvk, registration, acc, start_page, pw_recovery
+import land, town, problem, login, loginvk, registration, acc, start_page, pw_recovery, policy
 from config import config, ROOT
 
 cfg_static = config.get('static')
