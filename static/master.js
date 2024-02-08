a4a62a78f05468fa116fb2348c553d1127dda7d6
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-02-07 21:33:34 -0500

    Логика диалоговых окон

--- static/master.js
+++ static/master.js
@@ -1,25 +1,30 @@
 window.confirm_answer = confirm_answer// HACK make it available to non-modules, for a reason you’d better not think of
 
-function confirm_answer(ev) {
-	if (!confirm('Готовы отправить ответ? Передумать будет нельзя!'))
-		ev.preventDefault()
-	else
-		save_progress()
+for (let button of document.querySelectorAll(".notification_hint.no")) {
+	button.addEventListener('click', close_hint)
+	button.addEventListener('touchstart', close_hint)
 }
 
-function confirm_hint(ev) {
-	show_hint();
-	if (!confirm('Вы точно хотите получить подсказку? Её стоимость 1 квантик.'))
-		ev.preventDefault()
-	else
-		hint_save()
+for (let button of document.querySelectorAll(".notifcation_confirm.no")) {
+	button.addEventListener('click', close_confirm)
+	button.addEventListener('touchstart', close_confirm)
+}
+
+for (let button of document.querySelectorAll(".hint_box")) {
+	button.addEventListener('click', show_hint)
+	button.addEventListener('touchstart', show_hint)
+}
+
+for (let button of document.querySelectorAll(".submit_button")) {
+	button.addEventListener('click', show_confirm)
+	button.addEventListener('touchstart', show_confirm)
 }
 
 for (let form of document.querySelectorAll("form.problem"))
-	form.onsubmit = function(e){confirm_answer(e)}
+	form.onsubmit = function(e){save_progress()}
 
 for (let form of document.querySelectorAll("form.hint"))
-	form.onsubmit = function(e){confirm_hint(e)}
+	form.onsubmit = function(e){hint_save()}
 
 var progress = document.createElement('input');
 progress.type = 'hidden';
@@ -48,7 +53,7 @@ function save_progress(){
 }
 
 function show_hint(){
-	close_solution()
+	close_confirm()
 	let dialog = document.querySelector('.hint_notification')
 	block_nav()
 	let zone = document.createElement('div')
@@ -65,6 +70,28 @@ function close_hint() {
 		return;
 	zone.remove()
 	let dialog = document.querySelector('.hint_notification')
+	dialog.classList.remove('show')
+	//update_button()
+}
+
+function show_confirm() {
+	close_hint()
+	let dialog = document.querySelector('.confirm_notification')
+	block_nav()
+	let zone = document.createElement('div')
+	zone.classList.add('shadow')
+	document.body.append(zone)
+	if (!dialog.classList.contains('show'))
+		dialog.classList.add('show')
+	//update_button()
+}
+
+function close_confirm() {
+	let zone = document.querySelector('.shadow')
+	if (!zone)
+		return;
+	zone.remove()
+	let dialog = document.querySelector('.confirm_notification')
 	dialog.classList.remove('show')
 	//update_button()
 }
