b58385c637208884338bcd682e2953162fc1e6b3
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-02-14 15:01:34 -0500

    Правка порядка заголовка

--- user.py
+++ user.py
@@ -20,8 +20,8 @@ def display_banner(db):
 
 	yield '<div class="menu">'
 	yield '<div class="menu_item" id="info"><div>О турнире</div></div>'
-	yield '<div class="menu_item" id="team"><div>О нас</div></div>'
 	yield '<div class="menu_item" id="examples"><div>Примеры задач</div></div>'
+	yield '<div class="menu_item" id="team"><div>О нас</div></div>'
 	yield '<div class="menu_item" id="contacts"><div>Наши контакты</div></div>'
 	yield '</div>'
 
