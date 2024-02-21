c833a522d528ef60f6c92721b3e08930f9da78e6
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-02-20 23:36:41 -0500

    Добавление скрипта для апдейта дб

---
+++ db/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+dir="$(dirname "$(realpath "$0")")"
+postgres="$1"
+
+if [ -z "$postgres" ]; then
+	# read from config.toml
+	postgres="$(PYTHONPATH="$dir/.." python3 -c "from config import config; print(config['db']['url'])")"
+fi
+
+psql "$postgres" -1 -f "$dir/changetonewdb.sql"
+python "$dir/problems.py" "$postgres"
+psql "$postgres" -1 -f "$dir/assign-problems_new.sql"
+
