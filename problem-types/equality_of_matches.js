2f4ce1865d8765f866bedcbc56c13a1731b71770
dicher74 <dicher74@gmail.com>
2024-03-11 22:17:00 +0300

    Начало работы с интерактивом в задаче 'Равенство их спичек'

---
+++ problem-types/equality_of_matches.js
@@ -0,0 +1,119 @@
+function autoscroll(x, y) {
+	let add = 40
+	let [x_diff, y_diff] = [0, 0]
+	let [bott, right] = [window.innerHeight, window.innerWidth]
+	if (x < add)
+		x_diff = x - add
+	if (y < add)
+		y_diff = y - add
+	if (y > bott - add)
+		y_diff = y - bott + add
+	if (x > right - add)
+		x_diff = x - right + add
+	scrollBy(x_diff, y_diff)
+}
+
+function dist(obj1, obj2) {
+	let rect1 = obj1.getBoundingClientRect()
+	let rect2 = obj2.getBoundingClientRect()
+	let [x1, y1] = [(rect1.right + rect1.left) / 2, (rect1.bottom + rect1.top) / 2]
+	let [x2, y2] = [(rect2.right + rect2.left) / 2, (rect2.bottom + rect2.top) / 2]
+	console.log(x1, x2, y1, y2)
+	return (x2 - x1) ** 2 + (y1 - y2) ** 2
+}
+
+function update_best() {
+	$('rect').removeClass('best')
+	let obj = $('.targeted')[0]
+	let [best_ind, min_dist] = [0, 10 ** 18]
+	$('rect').each(function(ind){
+		let new_dist = dist(this, obj)
+		if (new_dist < min_dist) {
+			[best_ind, min_dist] = [ind, new_dist]
+		}
+	})
+	if (min_dist <= min_dist_) {
+		$(`rect:eq(${best_ind})`).addClass('best')
+	}
+}
+
+function start_move(e) {
+	if (e.touches)
+		e.preventDefault();
+	let obj = $(e.currentTarget)
+	$('svg').append(obj)
+	let svg = $('svg')[0].getBoundingClientRect()
+	$(obj).attr({
+			'x': e.clientX - svg.left - match_width / 2, 
+			'y': e.clientY - svg.top - match_length / 2,
+			'transform': 'rotate(0)'})
+	$(obj).addClass('targeted')
+	$(document).on('mousemove touchmove', move)
+}
+
+function move(e) {
+	if (e.touches)
+		e.preventDefault();
+	let obj = $('.targeted')
+	let svg = $('svg')[0].getBoundingClientRect()
+	$(obj).attr({
+			'x': e.clientX - svg.left - match_width / 2,
+			'y': e.clientY - svg.top - match_length / 2})
+	let [x, y] = [e.clientX, e.clientY]
+	if (x <= 0 || y <= 0 || y >= $(window).height() || x >= $(window).width()) {
+		drop()
+	}
+	else 
+		update_best()
+	console.log($(obj).attr('pos'), $(obj).attr('num'))
+	autoscroll(e.clientX, e.clientY)
+}
+
+function back_to_drag() {
+	let obj = $('.match.targeted:eq(0)')
+	let [pos, num] = [$(obj).attr('pos'), $(obj).attr('num')]
+	console.log(pos, num, $(obj).attr('x'), $(obj).attr('y'))
+	let rect = $(`rect[pos=${pos}][num=${num}]`)
+	$(obj).attr('x', $(rect).attr('x') - pad_)
+	$(obj).attr('y', $(rect).attr('y'))
+	if ($(rect).attr('direction') === 'hor') {
+		$(obj).attr('y', $(obj).attr('y') - -rect_width)
+	}
+	$(obj).appendTo(`g.num[num=${num}]`)
+}
+
+function drop() {
+	let obj = $('.targeted')
+	if ($('.best').length) {
+		let rect = $('.best')
+		let num = $(rect).attr('num')
+		$(obj).attr({
+			'x': $(rect).attr('x') - pad_,
+			'y': $(rect).attr('y') - -rect_width * ($(rect).attr('direction') === 'hor'),
+			'pos': $(rect).attr('pos'),
+			'num': $(rect).attr('num'),
+		})
+		if ($(rect).attr('direction') === 'hor')
+			$(obj).attr('transform', `rotate(-90 ${$(obj).attr('x') - -pad_} ${$(obj).attr('y')})`)
+		rect.removeClass('best')
+		$(obj).appendTo(`g.num[num=${num}]`)
+	}
+	else {
+		back_to_drag()
+	}
+	$(obj).removeClass('targeted')
+	$(document).off('mousemove touchmove')
+}
+
+const match_width = Math.min($('.match').attr('width'), $('.match').attr('height'))
+const match_length = Math.max($('.match').attr('width'), $('.match').attr('height'))
+const rect_width = $('rect').attr('height')
+const pad_ = (match_width - rect_width) / 2
+const min_dist_ = 900 //квадрат расстояния между rect и match для подстветки
+const side = {
+	'hor': [match_length, match_width],
+	'vert': [match_width, match_length]
+}
+
+$('.match.active').on('mousedown touchstart', start_move)
+$('.match.active').on('mouseup touchend', drop)
--- problem-types/equality_of_matches.py
+++ problem-types/equality_of_matches.py
@@ -1,9 +1,9 @@
 def entry_form(data, kwargs):
-	match_length = 47  #длина спички
+	match_length = 58  #длина спички
 	match_width = 7  #ширина спички
-	match_width_ = 11 #ширина картинки спички
+	match_width_ = 10 #ширина картинки спички
 	pad_ = (match_width_ - match_width) / 2  # сдвиг между картинкой и тенью
-	match_length_ = 47 # длина картинки спички
+	match_length_ = 58 # длина картинки спички
 	full_match = match_length + match_width  # длина с отступом для удобства
 	full_match_ = match_length_ + match_width_  # длина картинки с отступом для удобства
 	num_height = match_length * 2 + match_width * 3  #высота цифр
@@ -31,13 +31,13 @@ def entry_form(data, kwargs):
 	]  # общая разметка rect под цифры
 
 	num_grid = [
-		{'x':  match_width - pad_, 'y': match_width, 'width': match_width_, 'height': match_length_, 'transform': f'rotate(-90 {match_width} {match_width})'},
-		{'x': -pad_, 'y': match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
-		{'x': full_match - pad_, 'y': match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
-		{'x':  match_width - pad_, 'y': full_match + match_width, 'width': match_width_, 'height': match_length_, 'transform': f'rotate(-90 {match_width} {match_width + full_match})'},
-		{'x': -pad_, 'y': full_match + match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
-		{'x': full_match - pad_, 'y': full_match + match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
-		{'x':  match_width - pad_, 'y': full_match * 2 + match_width, 'width': match_width_, 'height': match_length_, 'transform': f'rotate(-90 {match_width} {full_match * 2 + match_width})'},
+		{'dir': 'hor', 'x':  match_width - pad_, 'y': match_width, 'width': match_width_, 'height': match_length_, 'transform': f'rotate(-90 {match_width} {match_width})'},
+		{'dir': 'vert', 'x': -pad_, 'y': match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
+		{'dir': 'vert', 'x': full_match - pad_, 'y': match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
+		{'dir': 'hor', 'x':  match_width - pad_, 'y': full_match + match_width, 'width': match_width_, 'height': match_length_, 'transform': f'rotate(-90 {match_width} {match_width + full_match})'},
+		{'dir': 'vert', 'x': -pad_, 'y': full_match + match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
+		{'dir': 'vert', 'x': full_match - pad_, 'y': full_match + match_width, 'width': match_width_, 'height': match_length_, 'transform': 'rotate(0)'},
+		{'dir': 'hor', 'x':  match_width - pad_, 'y': full_match * 2 + match_width, 'width': match_width_, 'height': match_length_, 'transform': f'rotate(-90 {match_width} {full_match * 2 + match_width})'},
 	]
 
 
@@ -72,16 +72,30 @@ def entry_form(data, kwargs):
 
 	yield f'<svg version="1.1" width="{view_box["width"]}" height="{view_box["height"]}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
 	
+	yield '<defs>'
+	yield '<filter id="sofGlow" height="110%" width="110%" x="-5%" y="-5%">'
+	yield '<feMorphology operator="dilate" radius="4" in="SourceAlpha" result="thicken" />'
+	yield '<feGaussianBlur in="thicken" stdDeviation="10" result="blurred" />'
+	yield '<feFlood flood-color="rgb(0,186,255)" result="glowColor" />'
+	yield '<feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored" />'
+	yield '<feMerge>'
+	yield '<feMergeNode in="softGlow_colored"/>'
+	yield '<feMergeNode in="SourceGraphic"/>'
+	yield '</feMerge>'
+	yield '</filter>'
+	yield '</defs>'
+
 	cur_dist = 0
 	yield f'<g class="field" transform="translate({margin["left"]} {margin["top"]})">'
 	for num_ind in range(num_amount):
-		yield f'<g class="num" transform="translate({cur_dist + symb_pad * (num_ind > 0)} {0})">'
+		yield f'<g class="num" num="{num_ind}" transform="translate({cur_dist + symb_pad * (num_ind > 0)} {0})">'
 		cur_dist += symb_pad * (num_ind > 0) + num_width
 		for ind in range(len(grid)):
-			yield f'<rect direction="{grid[ind][0]}" ind="{ind}" x="{grid[ind][1]}" y="{grid[ind][2]}" width="{grid[ind][3]}" height="{grid[ind][4]}" />'
+			yield f'<rect direction="{grid[ind][0]}" pos="{ind}" num="{num_ind}" x="{grid[ind][1]}" y="{grid[ind][2]}" width="{grid[ind][3]}" height="{grid[ind][4]}" />'
 		for ind in translate[data['nums'][num_ind]]:
 			rect = num_grid[ind]
-			yield f"""<image href="/static/problem_assets/match.svg" 
+			yield f"""<image class="match active" num="{num_ind}" pos="{ind}" href="/static/problem_assets/match.svg" 
+							direction="{rect["dir"]}"
 							x="{rect["x"]}" y="{rect["y"]}" 
 							width="{rect["width"]}" 
 							height="{rect["height"]}" 
@@ -91,7 +105,7 @@ def entry_form(data, kwargs):
 		if (num_ind < sgn_amount):
 			yield f'<g class="sgn" type="{data["sgn"][num_ind]}" transform="translate({cur_dist + match_length / 2 + symb_pad} {num_height / 2})">'
 			for rect in sgn_grid[data['sgn'][num_ind]]:
-				yield f"""<image href="/static/problem_assets/match.svg" 
+				yield f"""<image class="match passive" href="/static/problem_assets/match.svg" 
 							x="{rect["x"]}" y="{rect["y"]}" 
 							width="{rect["width"]}" 
 							height="{rect["height"]}" 
