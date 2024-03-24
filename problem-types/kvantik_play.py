b8f35869596d77de1b32dd97572abde030b92e57
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-03-24 14:10:39 -0400

    Добавление цвета в задачу Ноутик и Квантик развлекаются

--- problem-types/kvantik_play.py
+++ problem-types/kvantik_play.py
@@ -1,7 +1,7 @@
 def entry_form(data, kwargs):
 	in_column = 2
 	in_row = 7
-	side = 70
+	side = 105
 	line_width = 3
 	ind = line_width / 2
 	inner_side = side - line_width 
@@ -12,21 +12,20 @@ def entry_form(data, kwargs):
 	yield '<input name="answer" type="hidden" />'
 	yield '<div class="plot_area">'
 	yield f'<svg version="1.1" width="{board_width}" height="{board_height}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
-	for y in range(0, in_column + 1):
-		yield f'<line class="grid_line" x1="{ind}" y1 = "{ind + y * side}" x2="{ind + in_row * side}" y2="{ind + y * side}" stroke-width="{line_width}"/>'
-	for x in range(0, in_row + 1):
-		yield f'<line class="grid_line" x1="{ind + x * side}" y1 = "{ind}" x2="{ind + x * side}" y2="{ind + in_column * side}" stroke-width="{line_width}"/>'
+	#for y in range(0, in_column + 1):
+		#yield f'<line class="grid_line" x1="{ind}" y1 = "{ind + y * side}" x2="{ind + in_row * side}" y2="{ind + y * side}" stroke-width="{line_width}"/>'
+	#for x in range(0, in_row + 1):
+		#yield f'<line class="grid_line" x1="{ind + x * side}" y1 = "{ind}" x2="{ind + x * side}" y2="{ind + in_column * side}" stroke-width="{line_width}"/>'
 	for y in range(0, 1):
 		for x in range(0, in_row):	
-			yield f'<text class="field" x="{x * side + inner_side / 3}" y="{y * side + inner_side / 2 + inner_side / 3}" width="{inner_side}" height="{inner_side}">{data['word'][x]}</text>'
+			yield f'<text class="field color{x}" x="{x * side}" y="{(y + 1) * side}" width="{inner_side}" height="{inner_side}">{data['word'][x]}</text>'
 	for y in range(1, 2):
 		for x in range(0, in_row):	
 			yield f'<foreignObject x="{x * side + line_width}" y="{y * side + line_width}" width="{inner_side}" height="{inner_side}">'
 			yield f'<div xmlns="http://www.w3.org/1999/xhtml">'
-			yield f'<input class="fieldinput" type="number" maxlength="1" width="{inner_side}" height="{inner_side}">'
+			yield f'<input class="fieldinput" width="{inner_side}" height="{inner_side}" type="number" maxlength="1">'
 			yield f'</div>'
 			yield f'</foreignObject>'
-			#yield f'<text contentEditable="true" class="answer{x}" number="{x}" x="{x * side + inner_side / 3}" y="{y * side + inner_side / 2 + inner_side / 3}" width="{inner_side}" height="{inner_side}">{x}</text>'
 	yield '</svg>'
 	yield '</div>'
 
