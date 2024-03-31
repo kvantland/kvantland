a66ffb09bb7d42b50f7fac5e4c0c64ddfc5f9cd9
dicher74 <dicher74@gmail.com>
2024-04-01 01:04:02 +0300

    Добавлена Яндекс метрика

--- start_page.py
+++ start_page.py
@@ -29,6 +29,21 @@ def display_start_page(db):
 	yield '<link rel="stylesheet" type="text/css" href="/static/design/footer.css">'
 	yield '<link rel="stylesheet" type="text/css" href="/static/design/start_page.css">'
 	yield '<script type="module" src="/static/design/user.js"></script>'
+	yield """<script type="text/javascript" >
+	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	   m[i].l=1*new Date();
+	   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	   ym(96847675, "init", {
+	        clickmap:true,
+	        trackLinks:true,
+	        accurateTrackBounce:true
+	   });
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/96847675" style="position:absolute; left:-9999px;" alt="" /></div></noscript>"""
+
 	yield from user.display_banner(db)
 	yield '<div class="content_wrapper">'
 
