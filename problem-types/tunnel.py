23f2f5a622b5abe50160a81c891ed7ece1f10ae3
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-04-01 22:48:19 -0400

    Добавление поля в задаче тоннель

--- problem-types/tunnel.py
+++ problem-types/tunnel.py
@@ -3,13 +3,17 @@ def entry_form(data, kwargs):
     height = 8
     line_width = 2 # ширина линий 
     ind = line_width / 2 # отступ
-    side = 40 # длина стороны квадрата с границами
+    side = 60 # длина стороны квадрата с границами
     inner_side = side - line_width # длина стороны квадрата без границ 
     board_width = line_width + width * side + ind # длина стороны доски
     board_height = line_width + height * side + ind
+    rect_height = 26
+    rect_width = 38
+    cube_width = 22
     yield '<input name="answer" type="hidden" />'
     yield '<div class="plot_area">'
-    yield f'<svg version="1.1" width="{board_height}" height="{board_width + ind + inner_side}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+    yield f'<svg version="1.1" width="{board_height}" height="{board_width}" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+    yield f'<rect class="grass" width="{board_height}" height="{board_width}"/>'
     for y in range(0, height + 1):
         yield f'<line class="grid_line" x1="{ind}" y1 = "{ind + y * side}" x2="{ind + width * side}" y2="{ind + y * side}" stroke-width="{line_width}"/>'
     for x in range(0, width + 1):
@@ -23,9 +27,19 @@ def entry_form(data, kwargs):
     
     for a, b, c in data['board']:
         if a == 0:
-            yield f'<line class="inside_line not_choiced" pos="{a}" column="{b}" row="{c}" x1="{ind + b * side}" y1 = "{ind + c * side}" x2="{ind + (b + 1) * side}" y2="{ind + c * side}" stroke-width="{line_width}"/>'
+            if (b != 0):
+                yield f'<image class="cube" x = "{b * side - cube_width / 2}" y="{c * side - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/block11x11.png"/>'
+            yield f'<image class="inside_line not_choiced" pos="{a}" column="{b}" row="{c}" x = "{b * side + cube_width / 2}" y="{c * side - rect_height / 2}" height="{rect_height}" width="{rect_width}" href="/static/problem_assets/tiles/rect13x19.png"/>'
+            if (b != width - 1):
+                yield f'<image class="cube" x = "{b * side + rect_width + cube_width / 2}" y="{c * side - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/block11x11.png"/>'
+            #yield f'<line class="inside_line not_choiced" pos="{a}" column="{b}" row="{c}" x1="{ind + b * side}" y1 = "{ind + c * side}" x2="{ind + (b + 1) * side}" y2="{ind + c * side}" stroke-width="{line_width}"/>'
         else:
-            yield f'<line class="inside_line not_choiced" pos="{a}" column="{b}" row="{c}" x1="{ind + b * side}" y1 = "{ind + c * side}" x2="{ind + b * side}" y2="{ind + (c + 1) * side}" stroke-width="{line_width}"/>'
+            if (c != 0):
+                yield f'<image class="cube" x = "{b * side - cube_width / 2}" y="{c * side - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/block11x11.png"/>'
+            yield f'<image class="inside_line not_choiced" pos="{a}" column="{b}" row="{c}" x = "{b * side - rect_height / 2}" y="{c * side + cube_width / 2}" height="{rect_width}" width="{rect_height}" href="/static/problem_assets/tiles/rect19x13.png"/>'
+            if (c != height - 1):
+                yield f'<image class="cube" x = "{b * side - cube_width / 2}" y="{c * side + rect_width + cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/block11x11.png"/>'
+            #yield f'<line class="inside_line not_choiced" pos="{a}" column="{b}" row="{c}" x1="{ind + b * side}" y1 = "{ind + c * side}" x2="{ind + b * side}" y2="{ind + (c + 1) * side}" stroke-width="{line_width}"/>'
     yield '</svg>'
     yield '</div>'
 
