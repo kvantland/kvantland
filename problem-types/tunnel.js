c7ebb0a9f61ba8487c5070a98d088a54b147aa24
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-04-03 01:48:33 -0400

    Добавление метки для ответа в задаче туннель

--- problem-types/tunnel.js
+++ problem-types/tunnel.js
@@ -1,13 +1,25 @@
 var cur = null
+var width = $('.inside_line').attr('width')
+var height = $('.inside_line').attr('height')
+var svg_box = document.querySelector('svg');
 
 $('.inside_line').on('click touchstart', function(e){
 	if (e.touches)
 		e.preventDefault()
 	if (cur != null) {
 		$(cur).toggleClass('choiced not_choiced')
+		$('.cross').remove()
 	}
 	$(this).toggleClass('choiced not_choiced')
 	cur = this
+	svgNS = "http://www.w3.org/2000/svg"
+	var cross = document.createElementNS(svgNS, 'text');
+	cross.innerHTML = '&#x274C;';
+	cross.setAttribute('x', $(cur).attr('x') - -($(cur).attr('width') / 2));
+	cross.setAttribute('y', $(cur).attr('y') - -(2 * $(cur).attr('height') / 3));
+	cross.setAttribute('text-anchor', 'middle');
+	cross.classList.add('cross');
+	svg_box.appendChild(cross);
 })
 
 $('.submit_button').on('click touchstart', function(e){
