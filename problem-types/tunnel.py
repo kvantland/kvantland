45bb1d5fb9f1ce13a9d52120ad650154d2f78f47
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-04-02 23:17:15 -0400

    Добавление травяных границ

--- problem-types/tunnel.py
+++ problem-types/tunnel.py
@@ -18,12 +18,39 @@ def entry_form(data, kwargs):
         yield f'<line class="grid_line" x1="{ind}" y1 = "{ind + y * side}" x2="{ind + width * side}" y2="{ind + y * side}" stroke-width="{line_width}"/>'
     for x in range(0, width + 1):
         yield f'<line class="grid_line" x1="{ind + x * side}" y1 = "{ind}" x2="{ind + x * side}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
-    yield f'<line class="border_line" x1="{ind}" y1 = "{ind}" x2="{ind + width * side}" y2="{ind}" stroke-width="{line_width}"/>'
-    yield f'<line class="border_line" x1="{ind}" y1 = "{ind + height * side}" x2="{ind + width * side}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
-    yield f'<line class="border_line" x1="{ind}" y1 = "{ind}" x2="{ind}" y2="{ind + 6 * side}" stroke-width="{line_width}"/>'
-    yield f'<line class="border_line" x1="{ind}" y1 = "{ind + 7 * side}" x2="{ind}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
-    yield f'<line class="border_line" x1="{ind + width * side}" y1 = "{ind}" x2="{ind + width * side}" y2="{ind + 1 * side}" stroke-width="{line_width}"/>'
-    yield f'<line class="border_line" x1="{ind + width * side}" y1 = "{ind + 2 * side}" x2="{ind + width * side}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
+
+    yield f'<image class="border_cube" x = "{-cube_width / 2}" y="{-cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11_cornerleft.png"/>'
+    yield f'<image class="border_cube" x = "{width * side - cube_width / 2}" y="{-cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11_corner.png"/>'
+    for x in range(1, width):
+        yield f'<image class="border_cube" x = "{x * side - cube_width / 2}" y="{-cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11_shade.png"/>'
+    for x in range(0, width):
+        yield f'<image class="border_line" x = "{x * side + cube_width / 2}" y="{-rect_height / 2}" height="{rect_height}" width="{rect_width}" href="/static/problem_assets/tiles/grass13x19.png"/>'
+
+    yield f'<image class="border_cube" x = "{-cube_width / 2}" y="{side * width - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11_cornerleft.png"/>'
+    yield f'<image class="border_cube" x = "{width * side - cube_width / 2}" y="{side * width - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11_corner.png"/>'
+    for x in range(1, width):
+        yield f'<image class="border_cube" x = "{x * side - cube_width / 2}" y="{side * width - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11_shade.png"/>'
+    for x in range(0, width):
+        yield f'<image class="border_line" x = "{x * side + cube_width / 2}" y="{side * width - rect_height / 2}" height="{rect_height}" width="{rect_width}" href="/static/problem_assets/tiles/grass13x19.png"/>'
+
+    for y in range(1, width):
+        yield f'<image class="border_cube" x = "{- cube_width / 2}" y="{y * side - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11.png"/>'
+    for y in range(0, width):
+        if (y != 6):
+            yield f'<image class="border_line" x = "{-rect_height / 2}" y="{y * side + cube_width / 2}" height="{rect_width}" width="{rect_height}" href="/static/problem_assets/tiles/grass19x13.png"/>'
+
+    for y in range(1, width):
+        yield f'<image class="border_cube" x = "{width * side - cube_width / 2}" y="{y * side - cube_width / 2}" height="{cube_width}" width="{cube_width}" href="/static/problem_assets/tiles/grass11x11.png"/>'
+    for y in range(0, width):
+        if (y != 1):
+            yield f'<image class="border_line" x = "{width * side - rect_height / 2}" y="{y * side + cube_width / 2}" height="{rect_width}" width="{rect_height}" href="/static/problem_assets/tiles/grass19x13.png"/>'
+
+    #yield f'<line class="border_line" x1="{ind}" y1 = "{ind}" x2="{ind + width * side}" y2="{ind}" stroke-width="{line_width}"/>'
+    #yield f'<line class="border_line" x1="{ind}" y1 = "{ind + height * side}" x2="{ind + width * side}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
+    #yield f'<line class="border_line" x1="{ind}" y1 = "{ind}" x2="{ind}" y2="{ind + 6 * side}" stroke-width="{line_width}"/>'
+    #yield f'<line class="border_line" x1="{ind}" y1 = "{ind + 7 * side}" x2="{ind}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
+    #yield f'<line class="border_line" x1="{ind + width * side}" y1 = "{ind}" x2="{ind + width * side}" y2="{ind + 1 * side}" stroke-width="{line_width}"/>'
+    #yield f'<line class="border_line" x1="{ind + width * side}" y1 = "{ind + 2 * side}" x2="{ind + width * side}" y2="{ind + height * side}" stroke-width="{line_width}"/>'
     
     for a, b, c in data['board']:
         if a == 0:
