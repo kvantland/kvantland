cc5725a8e03a760c70ae0470d4791bacb4f6e90b
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-05-10 21:33:48 -0400

    Добавление страницы политики конфиденциальности

---
+++ client/modules/policy-page/ConfidentialityInfo/ConfidentialityInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="conf_content" id="confidentialityInfo">
+        <div class="page_header">Политика конфиденциальности</div>
+            <PolicyPar :parHeader="policyPar.header" :parDescription="policyPar.desc" 
+                        v-for="(policyPar, key) in policyPars" :key="key" />
+    </div>
+</template>
+
+<script>
+import PolicyPar from "./components/PolicyPar.vue"
+
+export default {
+    components: {
+        PolicyPar,
+    },
+
+    data() {
+        return {
+            policyPars: {},
+        }
+    },
+
+    async fetch() {
+        const policyParsData = await this.$axios.$get('/policy_pars')
+        this.policyPars = policyParsData
+    },
+}
+</script>
---
+++ client/modules/policy-page/ConfidentialityInfo/components/PolicyPar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="par">
+        <div class="header" v-html="parHeader"></div>
+        <div class="content" v-html="parDescription"> </div>
+	</div>
+</template>
+
+<script>
+export default {
+    props: ['parHeader', 'parDescription']
+}
+</script>
