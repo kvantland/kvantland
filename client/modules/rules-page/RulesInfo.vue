5107fc86ee2115b6d32273aa5cf8b1c5099e5487
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-05-14 20:52:04 -0400

    Добавление страницы и логики

---
+++ client/modules/rules-page/RulesInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="conf_content" id="rulesInfo">
+        <div class="page_header">Правила — Квантландия</div>
+            <RulesInfoCard :rulesDescription="rulesInfo.desc" 
+                        v-for="(rulesInfoCard, key) in rulesInfoCards" :key="key" />
+    </div>
+</template>
+
+<script>
+import RulesInfoCard from "./components/RulesInfoCard.vue"
+
+export default {
+    components: {
+        PolicyPar,
+    },
+
+    data() {
+        return {
+            rulesInfoCards: {},
+        }
+    },
+
+    async fetch() {
+        const rulesInfoCardData = await this.$axios.$get('/api/rules_card')
+        this.rulesInfoCards = rulesInfoCardData
+    },
+}
+</script>
---
+++ client/pages/rules.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <StartHeader />
+        <div class="content_wrapper">
+            <ConfidentialityInfo />
+        </div>
+    </div>
+</template>
+
+
+<script>
+import StartHeader from '../../modules/all-pages/Headers/StartHeader.vue'
+import ConfidentialityInfo from '../../modules/policy-page/ConfidentialityInfo/ConfidentialityInfo.vue'
+    export default {
+        head() {
+            return {
+                title: 'Квантландия'
+            };
+        },
+
+        components: {
+            StartHeader,
+            ConfidentialityInfo,
+        },
+    };
+</script>
+
+<style>
+@import "../../assets/css/rules.css";
+</style>
