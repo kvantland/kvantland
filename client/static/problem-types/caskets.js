9129fece69028de5a34ba2492487f7d6ff6d11e2
dicher74 <dicher74@gmail.com>
2024-05-27 17:22:35 +0300

    прежний код в caskets

--- client/static/problem-types/caskets.js
+++ client/static/problem-types/caskets.js
@@ -6,17 +6,17 @@ function open_casket(casket, config) {
 		return;
 	}
 	$(casket).attr('tmp', casket.getAttribute('tmp') - -1)
-	$(casket).attr('src', '/problem_assets/casket/' + config + '_' + $(casket).attr('tmp') + '.png') 
+	$(casket).attr('src', '/static/problem_assets/casket/' + config + '_' + $(casket).attr('tmp') + '.png') 
 }
 
 function send_xhr(params) {
 	let url = new URL(window.location.href + 'xhr')
 	let casket = params['casket']
-	$(casket).attr('src', '/problem_assets/casket/empty_6.png')
+	$(casket).attr('src', '/static/problem_assets/casket/empty_6.png')
 	let solution_empty = $('#problem_form')[0].outerHTML
-	$(casket).attr('src', '/problem_assets/casket/full_6.png')
+	$(casket).attr('src', '/static/problem_assets/casket/full_6.png')
 	let solution_full = $('#problem_form')[0].outerHTML
-	$(casket).attr('src', '/problem_assets/casket/empty_1.png')
+	$(casket).attr('src', '/static/problem_assets/casket/empty_1.png')
 	$.post(url, JSON.stringify({'answer': casket.getAttribute('num'), 'solution_empty': solution_empty, 'solution_full': solution_full}), function(data){
 		if (data == 'true')
 			movement = setInterval(function(){open_casket(casket, 'full')}, 30)
