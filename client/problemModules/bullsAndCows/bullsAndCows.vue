8e8c78a4d186111a42aa56a90e77ec8c9b918ca4
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-06-04 12:53:42 -0400

    Добавления отображения быков

--- client/problemModules/bullsAndCows/bullsAndCows.vue
+++ client/problemModules/bullsAndCows/bullsAndCows.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="plot_area">
-    <svg version="1.1" :width="`${boardWidth + paletteInd}`" 
+    <svg version="1.1" :width="`${boardWidth + animalInd}`" 
         :height="`${boardHeight}`" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <g class="board">
             <line class="horizontal" v-for="(line, num) in rectInColumn  + 1" 
@@ -14,6 +14,14 @@
                     :y="`${yInd * side + rectInd + side / 2}`"> {{ perimeterValue }} </text>
             </g>
         </g>
+        <g class="animals" :transform="`translate(${boardWidth + animalInd} ${side / 2})`">
+            <g class="animalColumns" v-for="(column, animalNum) in animalInRow" 
+                :transform="`translate(${animalNum * (animalColumnInd + 2 * 10) + 10} 0)`">
+                <image v-for="(animal, animalNum) in animalInColumn" href="problem_assets/bulls_and_cows/bull.png" class="animal" :y="`${animalNum * side + side}`" />
+                <circle v-for="(color, colorNum) in animalInColumn" :class="`color color-${colorNum + animalNum * animalInRow}`" cx="0" 
+                    :cy="`${colorNum * side + side}`" :r="10" @click="chooseColor(colorNum + columnNum * animalInRow)" />
+            </g>
+        </g>
     </svg>
     </div>
 </template>
@@ -24,14 +32,17 @@ export default {
     data() {
         const rectInRow = this.problemParams['start_board'][0].length
         const rectInColumn = this.problemParams['start_board'].length
+        const animalInRow = this.problemParams['cows_and_bulls'][0].length
+        const animalInColumn = this.problemParams['cows_and_bulls'].length
         return {
             rectInColumn: rectInColumn,
             rectInRow: rectInRow,
+            animalInRow: animalInRow,
+            animalInColumn: animalInColumn,
             side: 80,
             lineWidth: 2,
-            paletteInd: 120,
-            paletteColumns: 2,
-            paletteColumnInd: 40,
+            animalInd: 40,
+            animalColumnInd: 40,
         }
     },
 
