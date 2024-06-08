70bded1e5260a66add7a0588bfd11fe9765ead77
dicher74 <dicher74@gmail.com>
2024-06-08 21:02:54 +0300

    Убран лишний параметр из функции

--- client/static/problemModules/unusualNumber/unusualNumber.vue
+++ client/static/problemModules/unusualNumber/unusualNumber.vue
@@ -4,7 +4,7 @@
 			<div class="field" v-for='index in inRow' :key="`field_${index}`">
 				<input :class="`fieldInput_${index}`" v-model="ans[index]" type="text"
                     min="0" max="9" :maxlength="maxLength" :input-save-value="ans[index]" @keypress="handleKeyPress(index, $event)" 
-                    @keyup="handleKeyUp(index)">
+                    @keyup="handleKeyUp">
 			</div>
 		</div>
 	</div>  
@@ -39,7 +39,7 @@ export default {
 	        	}
 	      	}
 	    },
-        handleKeyUp(index) {
+        handleKeyUp() {
       		this.$emit('updateAnswer', parseInt(this.ans.join('')));
     	}
     },
