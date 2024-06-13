1ac866cd3cbd16c66e1b1ef51c1e87fba3a46520
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-06-13 13:48:11 -0400

    Добавление шаблона задачи Авиалинии

---
+++ client/static/problemModules/airlines/airlines.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    props: ['problemParams'],
+    model: {
+        prop: 'answer',
+        event: 'updateAnswer'
+    },
+    data() {
+        return {
+            startValues: this.problemParams.start_values
+        }
+    },
+    methods: {
+        changeConfig(newValue) {
+            let ans = []
+            for (const slot of newValue) {
+                ans.push(slot[1])
+            }
+            this.$emit('updateAnswer', ans)
+        }
+    }
+}
+</script>
--- db/problems.py
+++ db/problems.py
@@ -407,6 +407,22 @@ def CombiRepublic4(cur):
 				'componentType': "rearrangedWeights",
 				'inputType': "InteractiveTypeInput",
 			}
+			variants_list = add_variant_to_list(variants_list, "Переставленные гирьки", desc, json.dumps(cont))
+
+	problems_list = add_problem_to_list(problems_list, cur, "Республика Комби", 2, 'airlines', "Авиалинии")
+	for N, A in [
+			(9, 8),
+			(10, 9),
+			(11, 10),
+			(12, 11),
+			]:
+			desc = f"""В стране {N} городов, из каждого из которых есть не более четырёх авиалиний в другие города. 
+			Известно, что из любого города можно долететь в любой другой, сделав не более трёх пересадок. Какое наименьшее число авиалиний может быть в этой стране?"""
+			cont = {
+				'correct': A,
+				'componentType': "airlines",
+				'inputType': "InteractiveTypeInput",
+			}
 			variants_list = add_variant_to_list(variants_list, "Переставленные гирьки", desc, json.dumps(cont))	
 
 	add_list(problems_list, variants_list)
