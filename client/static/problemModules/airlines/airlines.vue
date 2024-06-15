8b033053c15ba0c404bb9ca464a1a921697c4372
Stigmsr <55955225+Stigmsr@users.noreply.github.com>
2024-06-14 21:41:42 -0400

    Добавление логики ответа

--- client/static/problemModules/airlines/airlines.vue
+++ client/static/problemModules/airlines/airlines.vue
@@ -2,8 +2,8 @@
     <div class="plot_area">
     <svg version="1.1" :width="svgWidth" 
         :height="svgHeight" overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <line v-for="(line, index) in lines" :key="index" :x1="line.x1" :y1="line.y1" :x2="line.x2" :y2="line.y2" stroke="black" />
-        <circle v-for="(point, index) in points" :key="index" :cx="point.x" :cy="point.y" :r="point.r" :class="`city_${index}`" @click="handleCircleClick(index)"/>
+        <line v-for="(line, index) in lines" :key="`line_${index}`" :x1="line.x1" :y1="line.y1" :x2="line.x2" :y2="line.y2" stroke="black" :class="`line_${index}`" />
+        <circle v-for="(point, index) in points" :key="`city_${index}`" :cx="point.x" :cy="point.y" :r="point.r" :class="`city_${index}`" @click="handleCircleClick(index)"/>
     </svg>
     </div>
 </template>
@@ -17,7 +17,7 @@ export default {
     },
     data() {
         const amount = this.problemParams['amount']
-        let cities = []
+        let ans = []
         return {
             amount: amount,
             side: 80,
@@ -29,6 +29,7 @@ export default {
             radius: 10,
             selectedPoints: [],
             lines: [],
+            ans: ans,
         }
     },
     methods: {
@@ -39,18 +40,33 @@ export default {
                     const [index1, index2] = this.selectedPoints;
                     const point1 = this.points[index1];
                     const point2 = this.points[index2];
-                    this.lines.push({
-                        x1: point1.x,
-                        y1: point1.y,
-                        x2: point2.x,
-                        y2: point2.y
-                    });
+                    if (!this.lineExists(point1, point2) && this.countLinesFromPoint(point1) < 4 && this.countLinesFromPoint(point2) < 4) {
+                        this.lines.push({
+                            x1: point1.x,
+                            y1: point1.y,
+                            x2: point2.x,
+                            y2: point2.y
+                        });
+                        this.ans.push([index1, index2]);
+                        this.$emit('updateAnswer', this.ans)
+                        console.log(this.ans)
+                    }
                     this.selectedPoints = [];
                 }
             } else {
                 this.selectedPoints = [index];
             }
-        }
+        },
+        lineExists(point1, point2) {
+            return this.lines.some(line => 
+                ((line.x1 === point1.x && line.y1 === point1.y && line.x2 === point2.x && line.y2 === point2.y) ||
+                (line.x1 === point2.x && line.y1 === point2.y && line.x2 === point1.x && line.y2 === point1.y))
+            );
+
+        },
+        countLinesFromPoint(point) {
+            return this.lines.filter(line => line.x1 === point.x && line.y1 === point.y).length;
+        },
     },
 
     computed: {
--- problem-types/airlines.py
+++ problem-types/airlines.py
@@ -1,2 +1,35 @@
+def build_graph(data):
+    graph = defaultdict(list)
+    for node1, node2 in data:
+        graph[node1].append(node2)
+        graph[node2].append(node1)
+    return graph
+
+def bfs_shortest_path(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = deque([start])
+    
+    while queue:
+        current = queue.popleft()
+        current_distance = distances[current]
+        
+        for neighbor in graph[current]:
+            if distances[neighbor] == float('inf'):
+                distances[neighbor] = current_distance + 1
+                queue.append(neighbor)
+    
+    return distances
+
 def validate(data, answer):
-	return data['correct'] == int(answer)
+    # Build the graph
+    graph = build_graph(answer)
+    
+    # Check connectivity and shortest path condition
+    for node in graph:
+        distances = bfs_shortest_path(graph, node)
+        for other_node in graph:
+            if distances[other_node] >= data['correct']:
+                return False
+    
+    return True
