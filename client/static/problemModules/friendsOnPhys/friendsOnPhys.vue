226437b1c7707dd1d5540714e47b2154ddbd76f0
dicher74 <dicher74@gmail.com>
2024-07-22 22:09:16 +0300

    Налажена механика в задаче 'Друзья на физкультуре'

--- client/static/problemModules/friendsOnPhys/friendsOnPhys.vue
+++ client/static/problemModules/friendsOnPhys/friendsOnPhys.vue
@@ -3,26 +3,29 @@
         preserveAspectRatio="xMidYMid meet" 
         overflow="visible" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <g class="answer_container" ref="ans_container">
-                <image class="answer_container" x="0" y="0" :width="answerAreaWidth" :height="answerAreaHeight" href="/problem_assets/friend_on_phys/field.png" />
-                <g class="boy" v-for="(boy, boyNum) in answerAreaBoys" @mousedown="moveFromAnsArea(boy)" @touchstart="moveFromAnsArea(boy)"
-                    :transform="`translate(${(boyGap + boyHeight) * boyNum + fieldGap} ${answerAreaHeight / 2})`" v-html="boy.html" />
+            <image class="answer_container" x="0" y="0" :width="answerAreaWidth" :height="answerAreaHeight" href="/problem_assets/friend_on_phys/field.png" />
+            <g class="answer_position" v-for="(boy, boyNum) in boysAmount" :transform="`translate(${(boyGap + boyHeight) * boyNum + fieldGap} ${answerAreaHeight / 2} )`">
+                <rect :class="['answer_rect', `rect_${boyNum}`, nearestRectNum == boyNum && !answerAreaBoys[nearestRectNum] ? 'active' : 'notActive']" :x="-boardWidth / 2" :y="-(boyHeight) / 2" :width="boyWidth" :height="boyHeight + boardHeight" />
+                <g class="boy" v-if="answerAreaBoys[boyNum]" @mousedown="moveFromAnsArea(answerAreaBoys[boyNum])" @touchstart="moveFromAnsArea(answerAreaBoys[boyNum])"
+                    v-html="answerAreaBoys[boyNum].html" />
+            </g>
         </g>
         <g class="drag_container" :transform="`translate(${answerAreaWidth} 0)`">
-                <g class="rows" :transform="`translate(0 ${boyGap})`">
-                    <g v-for="(row, rowNum) in rows" class="row" :transform="`translate(${boyGap} ${(boyGap + boyWidth) * rowNum})`">
-                        <g v-for="(boy, boyNum) in inRow" v-if="startAreaBoys[boyNum + rowNum * inRow]" 
-                            :transform="`translate(${boyCoordinates[boyNum + rowNum * inRow].x} ${boyCoordinates[boyNum + rowNum * inRow].y})`"
-                            @touchstart="moveFromStartArea(boyNum + rowNum * inRow, $event.touches[0])" 
-                            @mousedown="moveFromStartArea(boyNum + rowNum * inRow, $event)"
-                            :class="`boy boy_${boyNum + rowNum * inRow}`">
-                                <image x="0" y="0" :height="boyHeight" :width="boyWidth" :href="`/problem_assets/friend_on_phys/boy${boyNum + rowNum * inRow + 1}.png`" />
-                                <g class="board" :transform="`translate(0 ${boyHeight})`">
-                                    <image class="board" x="0" y="0" :width="boardWidth" :height="boardHeight" href="/problem_assets/board.svg" />
-                                    <text class="boardName" :x="boardWidth / 2" :y="boardHeight / 2" dy="0.35em"> {{ boardNames[boyNum + rowNum * inRow] }} </text>
-                                </g>
-                        </g>
+            <g class="rows" :transform="`translate(0 ${boyGap})`">
+                <g v-for="(row, rowNum) in rows" class="row" :transform="`translate(${boyGap} ${(boyGap + boyWidth) * rowNum})`">
+                    <g v-for="(boy, boyNum) in inRow" v-if="startAreaBoys[boyNum + rowNum * inRow]" 
+                        :transform="`translate(${boyCoordinates[boyNum + rowNum * inRow].x} ${boyCoordinates[boyNum + rowNum * inRow].y})`"
+                        @touchstart="moveFromStartArea(boyNum + rowNum * inRow, $event.touches[0])" 
+                        @mousedown="moveFromStartArea(boyNum + rowNum * inRow, $event)"
+                        :class="`boy boy_${boyNum + rowNum * inRow}`">
+                            <image x="0" y="0" :height="boyHeight" :width="boyWidth" :href="`/problem_assets/friend_on_phys/boy${boyNum + rowNum * inRow + 1}.png`" />
+                            <g class="board" :transform="`translate(0 ${boyHeight})`">
+                                <image class="board" x="0" y="0" :width="boardWidth" :height="boardHeight" href="/problem_assets/board.svg" />
+                                <text class="boardName" :x="boardWidth / 2" :y="boardHeight / 2" dy="0.35em"> {{ boardNames[boyNum + rowNum * inRow] }} </text>
+                            </g>
                     </g>
                 </g>
+            </g>
         </g>
         <g v-if="dragMode" class="choiced" :transform="`translate(${targetBoy.x} ${targetBoy.y})`" ref="choiced">           
             <image :x="-boyWidth / 2" :y="-boyHeight / 2" :height="boyHeight" :width="boyWidth" :href="`/problem_assets/friend_on_phys/boy${targetBoy.index+1}.png`" />
@@ -50,11 +53,12 @@ export default {
             inRow: 2,
             rows: 2,
             boyGap: 15,
-            fieldGap: 75,
             boyHeight: 140,
             boyWidth: 140,
             dropAllowDistance: 70,
             targetBoy: {x: 0, y:0, index: undefined},
+            nearestRectNum: undefined,
+            allowedDistToDrop: 100,
 
             startAreaBoys: {},   
             answerAreaBoys: [],
@@ -76,6 +80,9 @@ export default {
         },
         answerAreaHeight(){
             return this.dragAreaHeight
+        },
+        fieldGap() {
+            return (this.answerAreaWidth - (this.boyGap + this.boyWidth) * (this.boysAmount - 1)) / 2
         },
         svgHeight() {
             return this.dragAreaHeight
@@ -107,6 +114,25 @@ export default {
             if (targetY < 0 || targetY > window.innerHeight)
                 return false
             return true
+        },
+        getNearestRectNum(x, y) {
+            let minDist = 1000000000000
+            let optimalNum = null
+            for(let rectNum = 0; rectNum < this.boysAmount; rectNum++) {
+                let rect = document.querySelector(`.rect_${rectNum}`).getBoundingClientRect()
+                let rectCenterCoordinates = {
+                    x: rect.width / 2 + rect.left,
+                    y: rect.height / 2 + rect.top
+                }
+                let dist = Math.hypot(x - rectCenterCoordinates.x, y - rectCenterCoordinates.y)
+                if (dist < minDist) {
+                    minDist = dist
+                    optimalNum = rectNum
+                }
+            }
+            if (minDist <= this.allowedDistToDrop)
+                return optimalNum
+            return null
         },
         autoscroll() {
             const targetX = window.event.clientX
@@ -180,33 +206,39 @@ export default {
                 x = event.clientX
                 y = event.clientY
             }
+            this.nearestRectNum = this.getNearestRectNum(x, y)
             this.moveAt(x, y)
         },
         endDrag() {
             if (!this.dragMode) {
                 return
             }
-            const AnsContainerRect = this.$refs['ans_container'].getBoundingClientRect()
-            if (this.inRect(window.event.x, window.event.y, AnsContainerRect)) {
-                console.log('drop to ans area')
-                this.dropToAnsArea()
-            }
-            else {
-                console.log('drop to drag area')
+            if (this.nearestRectNum === undefined) {
                 this.backToStartArea()
             }
+            else {
+                if (this.answerAreaBoys[this.nearestRectNum]) 
+                    this.backToStartArea()
+                else
+                    this.dropToAnsArea()
+            }
             this.dragMode = false
+            this.nearestRectNum = undefined
         },
         dropToAnsArea() {
-            let newAnswerAreaBoys = JSON.parse(JSON.stringify(this.answerAreaBoys))
-            newAnswerAreaBoys.push({
+            const newAnswerAreaBoy = {
                 index: this.targetBoy.index,
                 html: this.$refs.choiced.innerHTML
-            })
-            console.log(newAnswerAreaBoys)
-            this.answerAreaBoys = newAnswerAreaBoys
+            }
+            console.log(newAnswerAreaBoy)
+            this.$set(this.answerAreaBoys, this.nearestRectNum, newAnswerAreaBoy)
+
             let newAnswerAreaBoysIndex = []
-            for (const boy of newAnswerAreaBoys) {
+            for (const boy of this.answerAreaBoys) {
+                if (!boy) {
+                    newAnswerAreaBoysIndex.push(-1)
+                    continue;
+                }
                 newAnswerAreaBoysIndex.push(boy.index + 1)
             }
             this.$emit('updateAnswer', newAnswerAreaBoysIndex)
@@ -223,13 +255,14 @@ export default {
         moveFromAnsArea(boy) {
             console.log(boy)
             let newAnswerAreaBoys = []
-            for (const answerBoy of this.answerAreaBoys) {
-                console.log(answerBoy.index, boy.index)
-                if (answerBoy.index != boy.index) {
-                    newAnswerAreaBoys.push(answerBoy)
+            for (let boyNum = 0; boyNum < this.boysAmount; boyNum++) {
+                if (!this.answerAreaBoys[boyNum]) {
+                    continue;
                 }
+                if (this.answerAreaBoys[boyNum].index == boy.index) {
+                    this.$set(this.answerAreaBoys, boyNum, undefined)
+                }   
             }
-            this.answerAreaBoys = newAnswerAreaBoys
             let newAnswerAreaBoysIndex = []
             for (const boy of newAnswerAreaBoys) {
                 newAnswerAreaBoysIndex.push(boy.index + 1)
