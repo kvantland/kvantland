c804ed69755fb5f2f7dad0c15573bc20b9459528
dicher74 <dicher74@gmail.com>
2024-09-24 19:51:36 +0300

    небольщие правки в land

--- client/pages/land.vue
+++ client/pages/land.vue
@@ -1,50 +1,53 @@
 <template>
-    <div>
-        <div class="content_wrapper_land">
-            <Breadcrumbs :crumbs="landCrumbs" />
-            <LandMap />
-        </div>
-    </div>
+	<div>
+		<div class="content_wrapper_land">
+			<Breadcrumbs :crumbs="landCrumbs" />
+			<LandMap />
+		</div>
+	</div>
 </template>
 
 
 <script>
 import LandMap from '../modules/land-page/LandMap.vue'
-    export default {
-        head() {
-            return {
-                title: 'Квантландия'
-            };
-        },
-        middleware: 'full-auth',
-        components: {
-            LandMap,
-        },
+export default {
+	
+	components: {
+		LandMap,
+	},
 
-        data() {
-            return {
-                landCrumbs: {},
-            }
-        },
+	middleware: 'full-auth',
 
-    async fetch() {
-        const landCrumbsData = await this.$axios.$get('/api/land_crumbs')
-        this.landCrumbs = landCrumbsData
-    },
+	data() {
+		return {
+			landCrumbs: {},
+		}
+	},
+
+	async fetch() {
+		const landCrumbsData = await this.$axios.$get('/api/land_crumbs')
+		this.landCrumbs = landCrumbsData
+	},
+
+	head() {
+		return {
+			title: 'Квантландия'
+		};
+	},	
 };
 </script>
 
 <style scoped>
 .content_wrapper_land {
-    width: 100%;
-    padding: 0 min(5vw, 80px) 80px min(5vw, 80px);
-    margin-top: 130px;
-    box-sizing: border-box;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 40px;
-    display: inline-flex;
+	width: 100%;
+	padding: 0 min(5vw, 80px) 80px min(5vw, 80px);
+	margin-top: 130px;
+	box-sizing: border-box;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	gap: 40px;
+	display: inline-flex;
 }
 
 </style>
