917304af164228da93eaf3ec97a2f4e067c529be
dicher74 <dicher74@gmail.com>
2024-09-25 21:42:50 +0300

    Уменьшен размер файла карты

--- client/modules/land-page/LandMap.vue
+++ client/modules/land-page/LandMap.vue
@@ -1,7 +1,7 @@
 <template>
 	<svg class="map" version="1.1" viewBox="0 0 1280 720" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 		<LandDefs/>
-		<image href="/map_img/land.svg" width="1280" height="720" preserveAspectRatio="xMinYMin" clip-path="url(#map_border)" meet/>
+		<image href="/map_img/land.png" width="1280" height="720" preserveAspectRatio="xMinYMin" clip-path="url(#map_border)" meet/>
 		<Towns/>
 	</svg>
 </template>
