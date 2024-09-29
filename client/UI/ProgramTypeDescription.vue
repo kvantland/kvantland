16dd3d8b85eb89ae82476c056ead0e5a85518047
dicher74 <dicher74@gmail.com>
2024-09-29 18:46:36 +0300

    добавлен общий компонент для описания задач по программированию

---
+++ client/UI/ProgramTypeDescription.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="description">
+		<slot />
+		<InputExamplesTable v-if=inputExamples.length :input-examples="inputExamples" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		inputExamples: {
+			type: Array,
+			default() {return []},
+		}
+	}
+}
+</script>
+
+<style scoped>
+.description {
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+}
+</style>
