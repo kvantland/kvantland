635fd0f9f2cab6140e9d200f5c6dc064e9f7578d
dicher74 <dicher74@gmail.com>
2025-01-26 22:45:07 +0300

    Восстановлена страница policy

---
+++ client/modules/policy-page/ConfidentialityInfo/ConfidentialityInfo.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="conf_content" id="confidentialityInfo">
+        <p class="page_header">Политика конфиденциальности</p>
+            <PolicyPar :parHeader="policyPar.header" :parDescription="policyPar.desc" :num="key"
+                        v-for="(policyPar, key) in policyPars" :key="key" />
+    </div>
+</template>
+
+<script>
+import PolicyPar from "./components/PolicyPar.vue"
+
+export default {
+    components: {
+        PolicyPar,
+    },
+
+    data() {
+        return {
+            policyPars: {},
+        }
+    },
+
+    async fetch() {
+        const policyParsData = await this.$axios.$get('/api/policy_pars')
+        this.policyPars = policyParsData
+    },
+}
+</script>
+
+<style scoped>
+.conf_content {
+    align-self: stretch;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    padding: 40px;
+    border-radius: 40px;
+    border: 3px #1E8B93 solid;
+}
+
+.conf_content .page_header {
+    color: #1E8B93;
+    font-size: 40px;
+    font-family: Montserrat Alternates;
+    font-weight: 600;
+    text-transform: capitalize;
+}
+
+.conf_content a {
+    color: #0000EE !important;
+    text-decoration: underline !important;
+}
+
+.conf_content a.visited {
+    color: #551A8B !important;
+}
+@media (max-width: 800px) {
+	.conf_content{
+		padding: 16px;
+		gap: 16px;
+	}	
+	.conf_content .page_header {
+		font-size: 20px;
+	}
+}
+</style>
---
+++ client/pages/policy/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="content_wrapper">
+        <ConfidentialityInfo />
+    </div>
+</template>
+
+
+<script>
+import ConfidentialityInfo from '../../modules/policy-page/ConfidentialityInfo/ConfidentialityInfo.vue'
+    export default {
+
+        components: {
+            ConfidentialityInfo,
+        },
+        head() {
+            return {
+                title: 'Квантландия'
+            };
+        },
+    };
+</script>
+
+<style scoped>
+.content_wrapper {
+    width: 100%;
+	padding-bottom: 80px;
+    box-sizing: border-box;
+    flex-direction: column;
+    justify-content: center;
+    display: inline-flex;
+}
+
+.content_wrapper * {
+    font-family: Montserrat;
+    word-wrap: break-word;
+    text-align: justify;
+}
+@media(max-width: 800px) {
+	.content_wrapper {
+		margin-top: 0px;
+		padding: 0px;
+		font-size: 12px;
+	}
+}
+</style>
